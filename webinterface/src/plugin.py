10e08db4a96172ae50e4bc15d93f5d5c4f788c94
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2007-01-19 00:24:59 +0000

    demonstration of a http-auth for the webif read my notes, before enabling! disabled by default

--- webinterface/src/plugin.py
+++ webinterface/src/plugin.py
@@ -6,6 +6,24 @@ sessions = [ ]
 DEBUG = False 
 DEBUGFILE= "/tmp/twisted.log"
 
+# Passwordprotection Test
+# set it only to True, if you have a patched wrapper.py
+# see http://twistedmatrix.com/trac/ticket/2041
+# in /usr/lib/python2.4/site-packages/twisted/web2/auth/wrapper.py
+# The solution is to change this line
+#	
+#	return self.authenticate(req), seg[1:]
+# into this
+#	return self.authenticate(req), seg
+PASSWORDPROTECTION = False
+PASSWORDPROTECTION_pwd = "root"
+PASSWORDPROTECTION_mode = "md5-sess"; 
+# twisted supports more than sha ('md5','md5-sess','sha')
+# but only sha works for me, but IE 
+# sha, Firefox=ok, Opera=ok, wget=ok, ie=not ok
+# md5-sess, firefox=not ok, opera=not ok,wget=ok, ie=not ok
+# md5 same as md5-sess 
+
 def startWebserver():
 	from twisted.internet import reactor
 	from twisted.web2 import server, channel, static, resource, stream, http_headers, responsecode, http
@@ -53,9 +71,39 @@ def startWebserver():
 		child_hdd = static.File("/hdd")
 		child_webdata = static.File(util.sibpath(__file__, "web-data")) # FIXME: web-data appears as webdata
 
-	site = server.Site(Toplevel())
-
+	if PASSWORDPROTECTION is False:
+		site = server.Site(Toplevel())
+	else:
+		from twisted.cred.portal import Portal
+        from twisted.cred import checkers
+        from twisted.web2.auth import digest, basic, wrapper
+        portal = Portal(HTTPAuthRealm())
+        checker = checkers.InMemoryUsernamePasswordDatabaseDontUse(root=PASSWORDPROTECTION_pwd)
+        portal.registerChecker(checker)
+        root = wrapper.HTTPAuthResource(Toplevel(),
+                                        (basic.BasicCredentialFactory('DM7025'),digest.DigestCredentialFactory(PASSWORDPROTECTION_mode,'DM7025')),
+                                        portal, (IHTTPUser,))
+        site = server.Site(root)
 	reactor.listenTCP(80, channel.HTTPFactory(site))
+
+# start classes for PASSWORDPROTECTION
+from zope.interface import Interface, implements
+from twisted.cred import portal
+class IHTTPUser(Interface):
+    pass
+
+class HTTPUser(object):
+    implements(IHTTPUser)
+
+class HTTPAuthRealm(object):
+    implements(portal.IRealm)
+
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        if IHTTPUser in interfaces:
+            return IHTTPUser, HTTPUser()
+
+        raise NotImplementedError("Only IHTTPUser interface is supported")
+# end  classes for PASSWORDPROTECTION
 
 def autostart(reason, **kwargs):
 	if "session" in kwargs:
