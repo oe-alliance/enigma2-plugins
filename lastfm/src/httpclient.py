d47e18dc1754ab230a938739e96386dcce8381e3
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2007-04-08 14:19:34 +0000

    removed syncron httpclient

--- lastfm/src/httpclient.py
+++ lastfm/src/httpclient.py
@@ -1,6 +1,3 @@
-from twisted.python.log import startLogging
-#startLogging(open("/tmp/twisted.log",'w'))
-
 from twisted.internet import reactor
 from twisted.internet.protocol import ClientFactory,connectionDone
 from twisted.web2.client.http import HTTPClientProtocol
@@ -25,14 +22,11 @@ class myProtocol(HTTPClientProtocol):
     def rawDataReceived(self,line):
         for l in line.split(self.delimiter): 
             if self.headerread:
-                #print "BODY",l
                 self.data +=l
             else:
                 if l == "":
-                    #print "END HEADER",l
                     self.headerread=True
                 else:
-                    #print "HEADER",l
                     self.header +=l
                 
     def connectionMade(self):
@@ -47,11 +41,12 @@ class myClientFactory(ClientFactory):
     initialDelay = 20
     maxDelay = 500
     
-    def __init__(self,hostname,path,method="GET",callback=None):
+    def __init__(self,hostname,path,method="GET",callback=None,errorback=None):
         self.hangup_ok = False
         self.path=path
         self.method=method
         self.callback=callback
+        self.errorback=errorback
         self.protocol = myProtocol(hostname,self.path,method=self.method)
         
     def startedConnecting(self, connector):
@@ -63,99 +58,19 @@ class myClientFactory(ClientFactory):
         if not self.hangup_ok:
             self.callback(self.protocol.data)
     def clientConnectionFailed(self, connector, reason):
-        print "Connection to host failed! (%s)" % reason.getErrorMessage()
+        if self.errorback is not None:
+            self.errorback(reason.getErrorMessage())
         ClientFactory.clientConnectionFailed(self, connector, reason)
 
 
 class testConn:
-    def __init__(self,hostname,port,path,method="GET",callback=None,errback=None):
-        print hostname
-        f = myClientFactory(hostname,path,method,callback)
+    def __init__(self,hostname,port,path,method="GET",callback=None,errorback=None):
+        f = myClientFactory(hostname,path,method,callback,errorback)
         try:
             hostname = socket.gethostbyname(hostname)
         except socket.error:
             msg = "address %r not found" % (hostname,)
-            if errback is not None:
-                errback(msg)
+            if errorback is not None:
+                errorback(msg)
         
         reactor.connectTCP(hostname, port, f)
-#########
-
-import base64
-
-import socket
-import string
-
-True = 1
-False = 0
-
-class httpclientDISABLED:
-
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.status = None
-        self.headers = None
-        self.response = None
-
-    def readline(self, s):
-        res = ""
-        while True:
-            try:
-                c = s.recv(1)
-            except:
-                break
-            res = res + c
-            if c == '\n':
-                break
-            if not c:
-                break
-        #print res
-        return res
-
-    def req(self, url):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(3)
-#        if config.useproxy:
-#            s.connect((config.proxyhost, config.proxyport))
-#            s.send("GET http://" + self.host + ":" + str(self.port) + url + " HTTP/1.0\r\n")
-#            if config.proxyuser != "":
-#                s.send("Proxy-Authorization: Basic " + base64.b64encode(config.proxyuser + ":" + config.proxypass) + "\r\n")
-#        else:
-#            print "reg: ",self.host, self.port,url
-            s.connect((self.host, self.port))
-            s.send("GET " + url + " HTTP/1.0\r\n")
-            s.send("Host: " + self.host + "\r\n")
-            s.send("\r\n")
-
-            line = self.readline(s)
-            #print line
-            self.status = string.rstrip(line)
-            
-            self.headers = {}
-            while True:
-                line = self.readline(s)
-                if not line:
-                    break
-                if line == "\r\n":
-                    break
-                tmp = string.split(line, ": ")
-                try:
-                  self.headers[tmp[0]] = string.rstrip(tmp[1])
-                except:
-                  print "BUG"
-                  print "self.headers[tmp[0]] = string.rstrip(tmp[1]) has no tmp[1]"
-                  print line
-                  print tmp              
-            self.response = ""
-            while True:
-                line = self.readline(s)
-                if not line:
-                    break
-                self.response = self.response + line
-            s.close()
-        except socket.error,e:
-            print e
-            self.response = ""
-            return False,e
