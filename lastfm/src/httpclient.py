b14c2d1fd179e5e854290d3ecc4833f5b4326f3c
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2007-04-08 12:54:57 +0000

    add asycron httpclient

--- lastfm/src/httpclient.py
+++ lastfm/src/httpclient.py
@@ -1,3 +1,85 @@
+from twisted.python.log import startLogging
+#startLogging(open("/tmp/twisted.log",'w'))
+
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory,connectionDone
+from twisted.web2.client.http import HTTPClientProtocol
+
+from twisted.internet import error 
+
+import socket
+
+class myProtocol(HTTPClientProtocol):
+    path = "/"
+    method = "GET"
+    data=""
+    header=""
+    headerread=False
+    def __init__(self,hostname,path,method="GET"):
+        self.path=path
+        self.method=method
+        self.hostname=hostname
+        HTTPClientProtocol.__init__(self)
+        self.setRawMode()
+        
+    def rawDataReceived(self,line):
+        for l in line.split(self.delimiter): 
+            if self.headerread:
+                #print "BODY",l
+                self.data +=l
+            else:
+                if l == "":
+                    #print "END HEADER",l
+                    self.headerread=True
+                else:
+                    #print "HEADER",l
+                    self.header +=l
+                
+    def connectionMade(self):
+        self.sendLine("%s %s HTTP/1.0"%(self.method,self.path))
+        self.sendLine("Host: %s"%self.hostname)
+        self.sendLine("User-Agent: enigma2 lastfm")
+        self.sendLine("")
+
+        
+class myClientFactory(ClientFactory):
+
+    initialDelay = 20
+    maxDelay = 500
+    
+    def __init__(self,hostname,path,method="GET",callback=None):
+        self.hangup_ok = False
+        self.path=path
+        self.method=method
+        self.callback=callback
+        self.protocol = myProtocol(hostname,self.path,method=self.method)
+        
+    def startedConnecting(self, connector):
+        pass
+    def buildProtocol(self, addr):
+        return self.protocol
+
+    def clientConnectionLost(self, connector, reason):
+        if not self.hangup_ok:
+            self.callback(self.protocol.data)
+    def clientConnectionFailed(self, connector, reason):
+        print "Connection to host failed! (%s)" % reason.getErrorMessage()
+        ClientFactory.clientConnectionFailed(self, connector, reason)
+
+
+class testConn:
+    def __init__(self,hostname,port,path,method="GET",callback=None,errback=None):
+        print hostname
+        f = myClientFactory(hostname,path,method,callback)
+        try:
+            hostname = socket.gethostbyname(hostname)
+        except socket.error:
+            msg = "address %r not found" % (hostname,)
+            if errback is not None:
+                errback(msg)
+        
+        reactor.connectTCP(hostname, port, f)
+#########
 
 import base64
 
@@ -7,7 +89,7 @@ import string
 True = 1
 False = 0
 
-class httpclient:
+class httpclientDISABLED:
 
     def __init__(self, host, port):
         self.host = host
