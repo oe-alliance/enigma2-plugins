5634c8aafe41ba443810427c59bdae4c5a7921c6
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2007-06-09 16:15:32 +0000

    add GrabResource avaible by http.//dm7025/grab this is an interface to Seddis AiO Dreambox Screengrabber

--- webinterface/src/plugin.py
+++ webinterface/src/plugin.py
@@ -19,6 +19,8 @@ import webif
 import WebIfConfig  
 import os
 
+from enigma import eConsoleAppContainer
+
 from Components.config import config, ConfigSubsection, ConfigInteger,ConfigYesNo,ConfigText
 
 config.plugins.Webinterface = ConfigSubsection()
@@ -123,6 +125,7 @@ def startWebserver():
 		child_webdata = static.File(util.sibpath(__file__, "web-data")) # FIXME: web-data appears as webdata
 		child_wap = static.File(util.sibpath(__file__, "wap")) # static pages for wap
 		child_movie = MovieStreamer()
+		child_grab = GrabResource()
 		def render(self, req):
 			fp = open(util.sibpath(__file__, "web-data")+"/index.html")
 			s = fp.read()
@@ -216,7 +219,69 @@ class MovieStreamer(resource.Resource):
 				return http.Response(responsecode.OK, stream="file was not found in /media/hdd/movie/")			
 		else:
 			return http.Response(responsecode.OK, stream="no file given with file=???")			
+#### 
+class GrabResource(resource.Resource):
+	"""
+		this is a interface to Seddis AiO Dreambox Screengrabber
+	"""
+	grab_bin = "/usr/bin/grab" 
+	grab_target = "/tmp/screenshot.bmp"
+	
+	def render(self, req):
+		class GrabStream(stream.ProducerStream):
+			def __init__(self,cmd,target=None,save=False):
+				self.cmd = cmd
+				self.target = target
+				self.save = save
+				self.output = ""
+				stream.ProducerStream.__init__(self)
+				
+				self.container = eConsoleAppContainer()
+				self.container.appClosed.get().append(self.cmdFinished)
+				self.container.dataAvail.get().append(self.dataAvail)
+				self.container.execute(cmd)
 
+			def cmdFinished(self,data):
+				if int(data) is 0 and self.target is not None:
+					fp = open(self.target)
+					self.write(fp.read())
+					fp.close()
+					if self.save is False:
+						os.remove(self.target)
+				elif int(data) is 0 and self.target is None:
+					self.write(self.output)
+				else:
+					self.write("internal error")
+				self.finish()	
+					
+			def dataAvail(self,data):
+				self.output += data
+			
+		if req.args.has_key("filename"):
+			filetarget = req.args['filename'][0]
+		else:
+			filetarget = self.grab_target
+		
+		if req.args.has_key("save"):
+			save_image = True
+		else:
+			save_image = False
+		
+		if os.path.exists(self.grab_bin) is not True:
+			return	http.Response(responsecode.OK,stream="grab is not installed at '%s'. go and fix it."%self.grab_bin)
+		elif req.args.has_key("command"): 
+			cmd = req.args['command'][0].replace("-","")
+			if cmd == "o":
+				return http.Response(responsecode.OK,stream=GrabStream(self.grab_bin+" -o "+filetarget,target=filetarget,save=save_image))
+			elif cmd == "v":
+				return http.Response(responsecode.OK,stream=GrabStream(self.grab_bin+" -v "+filetarget,target=filetarget,save=save_image))
+			elif cmd == "":
+				return http.Response(responsecode.OK,stream=GrabStream(self.grab_bin+" "+filetarget,target=filetarget,save=save_image))
+			else:
+				return http.Response(responsecode.OK,stream=GrabStream(self.grab_bin+" -h"))
+		else:
+			return http.Response(responsecode.OK,stream=GrabStream(self.grab_bin+" "+filetarget,target=filetarget,save=save_image))
+####		
 def autostart(reason, **kwargs):
 	if "session" in kwargs:
 		global sessions
