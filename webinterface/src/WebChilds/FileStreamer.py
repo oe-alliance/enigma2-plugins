4fd7fd986a0ab4e75f9d94a30d586f8199d71939
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2007-07-14 12:41:45 +0000

    python-large-file support is now avaiable in OE, so we dont need to work around the stream.FileStream-Exception
    Its now possible to seek to moviepositions in files >2GB also
    
    increase depending enigma2-version, so that it is garantied that we install it in an actual image with this oe-fixes

--- webinterface/src/WebChilds/FileStreamer.py
+++ webinterface/src/WebChilds/FileStreamer.py
@@ -5,55 +5,6 @@ class FileStreamer(resource.Resource):
     addSlash = True
     
     def render(self, req):
-        class myFileStream(stream.FileStream):
-            """
-                because os.fstat(f.fileno()).st_size returns negative values on 
-                large file, we set read() to a fix value
-            """
-            readsize = 10000    
-            
-            def read(self, sendfile=False):
-                if self.f is None:
-                    return None
-        
-                length = self.length
-                if length == 0:
-                    self.f = None
-                    return None
-        
-                if sendfile and length > SENDFILE_THRESHOLD:
-                    # XXX: Yay using non-existent sendfile support!
-                    # FIXME: if we return a SendfileBuffer, and then sendfile
-                    #        fails, then what? Or, what if file is too short?
-                    readSize = min(length, SENDFILE_LIMIT)
-                    res = SendfileBuffer(self.f, self.start, readSize)
-                    self.length -= readSize
-                    self.start += readSize
-                    return res
-        
-                if self.useMMap and length > MMAP_THRESHOLD:
-                    readSize = min(length, MMAP_LIMIT)
-                    try:
-                        res = mmapwrapper(self.f.fileno(), readSize,
-                                          access=mmap.ACCESS_READ, offset=self.start)
-                        #madvise(res, MADV_SEQUENTIAL)
-                        self.length -= readSize
-                        self.start += readSize
-                        return res
-                    except mmap.error:
-                        pass
-                # Fall back to standard read.
-                readSize = self.readsize #this is the only changed line :} 3c5x9 #min(length, self.CHUNK_SIZE)
-                
-                self.f.seek(self.start)
-                b = self.f.read(readSize)
-                bytesRead = len(b)
-                if not bytesRead:
-                    raise RuntimeError("Ran out of data reading file %r, expected %d more bytes" % (self.f, length))
-                else:
-                    self.length -= bytesRead
-                    self.start += bytesRead
-                    return b
         try:
             w1 = req.uri.split("?")[1]
             w2 = w1.split("&")
@@ -70,8 +21,7 @@ class FileStreamer(resource.Resource):
             filename = parts["file"].replace("%20"," ")
             path = root+filename
             if os.path.exists(path):
-                self.filehandler = open(path,"r")
-                s = myFileStream(self.filehandler)
+                s = stream.FileStream(open(path,"r"))
                 type = path.split(".")[-1]
                 header = http_headers.MimeType('video', 'ts')
                 if type == "mp3" or type == "ogg" or type == "wav":
