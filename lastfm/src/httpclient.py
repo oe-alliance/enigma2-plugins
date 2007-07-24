9b3129351e92c67c3bd88cbb7bf75a1da2993216
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2007-07-24 19:09:10 +0000

    re-add used functions

--- lastfm/src/httpclient.py
+++ lastfm/src/httpclient.py
@@ -3,6 +3,7 @@ from twisted.web2.client.http import HTTPClientProtocol
 from twisted.internet import reactor, error
 from urlparse import urlsplit
 from socket import gethostbyname
+from urllib import urlencode as urllib_urlencode, quote_plus as urllib_quote_plus
 
 global HTTPCLIENT_requestCount
 HTTPCLIENT_requestCount = 0 # counts requests
@@ -174,6 +175,12 @@ class Enigma2HTTPClientFactory(ClientFactory):
     def clientConnectionFailed(self, connector, reason):
         self.request.RequestError(reason.getErrorMessage())
         ClientFactory.clientConnectionFailed(self, connector, reason)
+
+def urlencode(dict):
+	return urllib_urlencode(dict)
+	
+def quote_plus(data):
+	return urllib_quote_plus(data)
 
 def getURL(url,callback=None,errorback=None,headercallback=None,method="GET",headers={}):
     """ 
