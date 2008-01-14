0e1435db56a1873c6cb4ddcf0aa20bf8aa50734f
Marcus Groß <fireball@users.schwerkraft.elitedvb.net>
2008-01-14 21:44:52 +0000

    keep it simple. don't store unneeded parameters.

--- startuptostandby/src/plugin.py
+++ startuptostandby/src/plugin.py
@@ -8,7 +8,7 @@ from enigma import eTimer
 config.plugins.startuptostandby = ConfigSubsection()
 config.plugins.startuptostandby.enabled = ConfigEnableDisable(default = False)
 timer = eTimer()
-savedkwargs = {}
+session = None
 
 def main(session, **kwargs):
 	print "[StartupToStandby] Open Config Screen"
@@ -17,12 +17,11 @@ def main(session, **kwargs):
 # Autostart
 def autostart(reason, **kwargs):
 	global timer
-	global savedkwargs
+	global session
 
 	print "[StartupToStandby] autostart"
 	if config.plugins.startuptostandby.enabled.value and reason == 0 and kwargs.has_key("session"):
 		session = kwargs["session"]
-		savedkwargs = kwargs
 		session.open(Standby)
 		#wait 10 seconds before setting standby again - bad hack...
 		print "[StartupToStandby] start timer..."
@@ -31,14 +30,14 @@ def autostart(reason, **kwargs):
 		print "[StartupToStandby] ...ready"
 
 def timeout():
-	global savedkwargs
+	global session
 	print "[StartupToStandby] Timeout!"
 	#standby-screen is open - close it
 	print "[StartupToStandby] Close Standby Screen"
-	savedkwargs["session"].open(Standby)
+	session.open(Standby)
 	#and open it again...
 	print "[StartupToStandby] Open Standby Screen"
-	savedkwargs["session"].open(Standby)
+	session.open(Standby)
 	
 	
 def Plugins(path, **kwargs):
