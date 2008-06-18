ab54f44217c5ec53c412d9695be44a1814296210
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2008-06-18 19:05:24 +0000

    fixing wrong zPosition in Skin

--- webinterface/src/WebIfConfig.py
+++ webinterface/src/WebIfConfig.py
@@ -19,13 +19,13 @@ class WebIfConfigScreen(ConfigListScreen,Screen):
         <screen position="100,100" size="550,400" title="Webinterface: Main Setup" >
             <widget name="config" position="0,0" size="550,360" scrollbarMode="showOnDemand" />
             
-            <widget name="key_red" position="0,360" size="60,40" valign="center" halign="center" zPosition="2"  foregroundColor="white" font="Regular;18"/> 
-            <widget name="key_yellow" position="140,360" size="60,40" valign="center" halign="center" zPosition="2"  foregroundColor="white" font="Regular;18"/> 
-            <widget name="key_green" position="280,360" size="60,40" valign="center" halign="center" zPosition="2"  foregroundColor="white" font="Regular;18"/>
+            <widget name="key_red" position="0,360" size="60,40" valign="center" halign="center" zPosition="4"  foregroundColor="white" font="Regular;18"/> 
+            <widget name="key_yellow" position="140,360" size="60,40" valign="center" halign="center" zPosition="4"  foregroundColor="white" font="Regular;18"/> 
+            <widget name="key_green" position="280,360" size="60,40" valign="center" halign="center" zPosition="4"  foregroundColor="white" font="Regular;18"/>
             
-            <ePixmap name="red"    position="0,360"   zPosition="4" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
-            <ePixmap name="green"  position="140,360" zPosition="4" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
-            <ePixmap name="yellow" position="280,360" zPosition="4" size="140,40" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" /> 
+            <ePixmap name="red"    position="0,360"   zPosition="2" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
+            <ePixmap name="green"  position="140,360" zPosition="2" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
+            <ePixmap name="yellow" position="280,360" zPosition="2" size="140,40" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" /> 
         </screen>"""
     def __init__(self, session, args = 0):
         self.session = session
