a90ec6522c9f9d16c129fc4b5c14bf34718e6f24
Moritz Venn <ritzmo@users.schwerkraft.elitedvb.net>
2008-07-04 19:55:31 +0000

    rearrange this a little...

--- autotimer/src/AutoTimerComponent.py
+++ autotimer/src/AutoTimerComponent.py
@@ -7,73 +7,29 @@ from re import compile as re_compile
 class AutoTimerComponent(object):
 	"""AutoTimer Component which also handles validity checks"""
 
-	def __init__(self, id, *args, **kwargs):
+	"""
+	 Initiate
+	"""
+	def __init__(self, id, name, match, enabled, *args, **kwargs):
 		self.id = id
 		self._afterevent = []
-		self.setValues(*args, **kwargs)
+		self.setValues(name, match, enabled, *args, **kwargs)
 
-	def __eq__(self, other):
-		try:
-			return self.id == other.id
-		except AttributeError:
-			return False
-
-	def __ne__(self, other):
-		return not self.__eq__(other)
-
-	def __copy__(self):
-		return self.__class__(
-			self.id,
-			self.name,
-			self.match,
-			self.enabled,
-			timespan = self.timespan,
-			services = self.services,
-			offset = self.offset,
-			afterevent = self.afterevent,
-			exclude = (self.getExcludedTitle(), self.getExcludedShort(), self.getExcludedDescription(), self.getExcludedDays()),
-			include = (self.getIncludedTitle(), self.getIncludedShort(), self.getIncludedDescription(), self.getIncludedDays()),
-			maxduration = self.maxduration,
-			destination = self.destination,
-			matchCount = self.matchCount,
-			matchLeft = self.matchLeft,
-			matchLimit = self.matchLimit,
-			matchFormatString = self.matchFormatString,
-			lastBegin = self.lastBegin,
-			justplay = self.justplay,
-			avoidDuplicateDescription = self.avoidDuplicateDescription,
-			bouquets = self.bouquets,
-			tags = self.tags
-		)
-
-	def __deepcopy__(self, memo):
-		return self.__class__(
-			self.id,
-			self.name,
-			self.match,
-			self.enabled,
-			timespan = self.timespan,
-			services = self.services[:],
-			offset = self.offset and self.offset[:],
-			afterevent = self.afterevent[:],
-			exclude = (self.getExcludedTitle(), self.getExcludedShort(), self.getExcludedDescription(), self.exclude[3][:]),
-			include = (self.getIncludedTitle(), self.getIncludedShort(), self.getIncludedDescription(), self.include[3][:]),
-			maxduration = self.maxduration,
-			destination = self.destination,
-			matchCount = self.matchCount,
-			matchLeft = self.matchLeft,
-			matchLimit = self.matchLimit,
-			matchFormatString = self.matchFormatString,
-			lastBegin = self.lastBegin,
-			justplay = self.justplay,
-			avoidDuplicateDescription = self.avoidDuplicateDescription,
-			bouquets = self.bouquets[:],
-			tags = self.tags[:]
-		)
-
+	"""
+	 Create a deep copy of this instance 
+	"""
 	def clone(self):
 		return self.__deepcopy__({})
 
+	"""
+	 Hook needed for WebIf
+	"""
+	def getEntry(self):
+		return self
+
+	"""
+	 Keeps init small and helps setting many values at once
+	"""
 	def setValues(self, name, match, enabled, timespan = None, services = None, offset = None, \
 			afterevent = [], exclude = None, maxduration = None, destination = None, \
 			include = None, matchCount = 0, matchLeft = 0, matchLimit = '', matchFormatString = '', \
@@ -81,15 +37,15 @@ class AutoTimerComponent(object):
 			tags = None):
 		self.name = name
 		self.match = match
+		self.enabled = enabled
 		self.timespan = timespan
 		self.services = services
 		self.offset = offset
 		self.afterevent = afterevent
 		self.exclude = exclude
-		self.include = include
 		self.maxduration = maxduration
-		self.enabled = enabled
 		self.destination = destination
+		self.include = include
 		self.matchCount = matchCount
 		self.matchLeft = matchLeft
 		self.matchLimit = matchLimit
@@ -99,38 +55,34 @@ class AutoTimerComponent(object):
 		self.avoidDuplicateDescription = avoidDuplicateDescription
 		self.bouquets = bouquets
 		self.tags = tags or []
-	
-	def getName(self):
-		return self.name
-	
-	def setName(self, name):
-		self.name = name
-	
-	def getMatch(self):
-		return self.match
-	
-	def setMatch(self, match):
-		self.match = match
-	
-	def getEntry(self):
-		return self
-	
-	def calculateDayspan(self, begin, end, ignore = None):
-		if end[0] < begin[0] or (end[0] == begin[0] and end[1] <= begin[1]):
-			return (begin, end, True)
+
+### Attributes / Properties
+
+	def getCompleteAfterEvent(self):
+		return self._afterevent
+
+	def setAfterEvent(self, afterevent):
+		del self._afterevent[:]
+		if len(afterevent):
+			for definition in afterevent:
+				action, timespan = definition
+				if timespan is None:
+					self._afterevent.append((action, (None,)))
+				else:
+					self._afterevent.append((action, self.calculateDayspan(*timespan)))
+
+	afterevent = property(getCompleteAfterEvent, setAfterEvent)
+
+	def setBouquets(self, bouquets):
+		if bouquets:
+			self._bouquets = bouquets
 		else:
-			return (begin, end, False)
+			self._bouquets = []
 
-	def setTimespan(self, timespan):
-		if timespan is None or len(timespan) and timespan[0] is None:
-			self._timespan = (None,)
-		else:
-			self._timespan = self.calculateDayspan(*timespan)
+	def getBouquets(self):
+		return self._bouquets
 
-	def getTimespan(self):
-		return self._timespan
-
-	timespan = property(getTimespan, setTimespan)
+	bouquets = property(getBouquets, setBouquets)
 
 	def setExclude(self, exclude):
 		if exclude:
@@ -164,52 +116,86 @@ class AutoTimerComponent(object):
 
 	include = property(getInclude, setInclude)
 
+	def setName(self, name):
+		self.name = name
+
+	def getMatch(self):
+		return self._match
+
+	def setMatch(self, match):
+		# XXX: a sanity check might be useful...
+		self._match = match
+
+	match = property(getMatch, setMatch)
+
+	def getServices(self):
+		return self._services
+
 	def setServices(self, services):
 		if services:
 			self._services = services
 		else:
 			self._services = []
 
-	def getServices(self):
-		return self._services
-
 	services = property(getServices, setServices)
 
-	def setBouquets(self, bouquets):
-		if bouquets:
-			self._bouquets = bouquets
+	def getTimespan(self):
+		return self._timespan
+
+	def setTimespan(self, timespan):
+		if timespan is None or len(timespan) and timespan[0] is None:
+			self._timespan = (None,)
 		else:
-			self._bouquets = []
+			self._timespan = self.calculateDayspan(*timespan)
 
-	def getBouquets(self):
-		return self._bouquets
+	timespan = property(getTimespan, setTimespan)
 
-	bouquets = property(getBouquets, setBouquets)
+### See if Attributes are set
 
-	def setAfterEvent(self, afterevent):
-		del self._afterevent[:]
-		if len(afterevent):
-			for definition in afterevent:
-				action, timespan = definition
-				if timespan is None:
-					self._afterevent.append((action, (None,)))
-				else:
-					self._afterevent.append((action, self.calculateDayspan(*timespan)))
+	def hasAfterEvent(self):
+		return len(self.afterevent)
 
-	def getCompleteAfterEvent(self):
-		return self._afterevent
+	def hasAfterEventTimespan(self):
+		for afterevent in self.afterevent:
+			if afterevent[1][0] is not None:
+				return True
+		return False
 
-	afterevent = property(getCompleteAfterEvent, setAfterEvent)
+	def hasCounter(self):
+		return self.matchCount != 0
+
+	def hasCounterFormatString(self):
+		return self.matchFormatString != ''
+
+	def hasDestination(self):
+		return self.destination is not None
+
+	def hasDuration(self):
+		return self.maxduration is not None
+
+	def hasTags(self):
+		return len(self.tags) > 0
 
 	def hasTimespan(self):
 		return self.timespan[0] is not None
 
-	def getTimespanBegin(self):
-		return '%02d:%02d' % (self.timespan[0][0], self.timespan[0][1])
+	def hasOffset(self):
+		return self.offset is not None
 
-	def getTimespanEnd(self):
-		return '%02d:%02d' % (self.timespan[1][0], self.timespan[1][1])
+### Helper
 
+	"""
+	 Returns a tulple of (input begin, input end, begin earlier than end)
+	"""
+	def calculateDayspan(self, begin, end, ignore = None):
+		if end[0] < begin[0] or (end[0] == begin[0] and end[1] <= begin[1]):
+			return (begin, end, True)
+		else:
+			return (begin, end, False)
+
+	"""
+	 Returns if a given timestruct is in a timespan
+	"""
 	def checkAnyTimespan(self, time, begin = None, end = None, haveDayspan = False):
 		if begin is None:
 			return False
@@ -236,20 +222,9 @@ class AutoTimerComponent(object):
 				return True
 			return False
 
-	def checkTimespan(self, begin):
-		return self.checkAnyTimespan(begin, *self.timespan)
-
-	def hasDuration(self):
-		return self.maxduration is not None
-
-	def getDuration(self):
-		return self.maxduration/60
-
-	def checkDuration(self, length):
-		if self.maxduration is None:
-			return False
-		return length > self.maxduration
-
+	"""
+	 Returns a list of all allowed services by listing the bouquets
+	"""
 	def getFullServices(self):
 		list = self.services[:]
 
@@ -276,22 +251,60 @@ class AutoTimerComponent(object):
 
 		return list
 
-	def checkServices(self, service):
-		if len(self.services) or len(self.bouquets): 
-			return service not in self.getFullServices()
-		return False
+	"""
+	 Called when a timer based on this component was added
+	"""
+	def update(self, begin, timestamp):
+		# Only update limit when we have new begin
+		if begin > self.lastBegin:
+			self.lastBegin = begin
 
-	def getExcludedTitle(self):
-		return [x.pattern for x in self.exclude[0]]
+			# Update Counter:
+			# %m is Month, %U is week (sunday), %W is week (monday)
+			newLimit = strftime(self.matchFormatString, timestamp)
 
-	def getExcludedShort(self):
-		return [x.pattern for x in self.exclude[1]]
+			if newLimit != self.matchLimit:
+				self.matchLeft = self.matchCount
+				self.matchLimit = newLimit
+
+### Makes saving Config easier
+
+	def getAvoidDuplicateDescription(self):
+		return self.avoidDuplicateDescription
+
+	def getCounter(self):
+		return self.matchCount
+
+	def getCounterFormatString(self):
+		return self.matchFormatString
+
+	def getCounterLeft(self):
+		return self.matchLeft
+
+	def getCounterLimit(self):
+		return self.matchLimit
+
+	def getDestination(self):
+		# XXX: as this function was not added by me (ritzMo) i'll leave it like this but i'm not really sure if this is right ;-)
+		return self.destination is not None
+
+	def getDuration(self):
+		return self.maxduration/60
+
+	def getEnabled(self):
+		return self.enabled and "yes" or "no"
+
+	def getExcludedDays(self):
+		return self.exclude[3]
 
 	def getExcludedDescription(self):
 		return [x.pattern for x in self.exclude[2]]
 
-	def getExcludedDays(self):
-		return self.exclude[3]
+	def getExcludedShort(self):
+		return [x.pattern for x in self.exclude[1]]
+
+	def getExcludedTitle(self):
+		return [x.pattern for x in self.exclude[0]]	
 
 	def getIncludedTitle(self):
 		return [x.pattern for x in self.include[0]]
@@ -305,6 +318,60 @@ class AutoTimerComponent(object):
 	def getIncludedDays(self):
 		return self.include[3]
 
+	def getJustplay(self):
+		return self.justplay and "1" or "0"
+
+	def getLastBegin(self):
+		return self.lastBegin
+
+	def getName(self):
+		return self.name
+
+	def getOffsetBegin(self):
+		return self.offset[0]/60
+
+	def getOffsetEnd(self):
+		return self.offset[1]/60
+
+	def getTags(self):
+		return self.tags
+
+	def getTimespanBegin(self):
+		return '%02d:%02d' % (self.timespan[0][0], self.timespan[0][1])
+
+	def getTimespanEnd(self):
+		return '%02d:%02d' % (self.timespan[1][0], self.timespan[1][1])
+
+	def isOffsetEqual(self):
+		return self.offset[0] == self.offset[1]
+	
+### Actual functionality
+
+	def applyOffset(self, begin, end):
+		if self.offset is None:
+			return (begin, end)
+		return (begin - self.offset[0], end + self.offset[1])
+
+	def checkCounter(self, timestamp):
+		# 0-Count is considered "unset"
+		if self.matchCount == 0:
+			return False
+
+		# Check if event is in current timespan (we can only manage one!)
+		limit = strftime(self.matchFormatString, timestamp)
+		if limit != self.matchLimit:
+			return True
+
+		if self.matchLeft > 0:
+			self.matchLeft -= 1
+			return False
+		return True
+
+	def checkDuration(self, length):
+		if self.maxduration is None:
+			return False
+		return length > self.maxduration
+	
 	def checkExcluded(self, title, short, extended, dayofweek):
 		if len(self.exclude[3]):
 			list = [x for x in self.exclude[3]]
@@ -325,6 +392,12 @@ class AutoTimerComponent(object):
 			if exclude.search(extended):
 				return True
 		return False
+
+	def checkFilter(self, title, short, extended, dayofweek):
+		if self.checkExcluded(title, short, extended, dayofweek):
+			return True
+
+		return self.checkIncluded(title, short, extended, dayofweek)
 
 	def checkIncluded(self, title, short, extended, dayofweek):
 		if len(self.include[3]):
@@ -348,43 +421,13 @@ class AutoTimerComponent(object):
 
 		return False
 
-	def checkFilter(self, title, short, extended, dayofweek):
-		if self.checkExcluded(title, short, extended, dayofweek):
-			return True
-
-		return self.checkIncluded(title, short, extended, dayofweek)
-
-	def hasOffset(self):
-		return self.offset is not None
-
-	def isOffsetEqual(self):
-		return self.offset[0] == self.offset[1]
-
-	def applyOffset(self, begin, end):
-		if self.offset is None:
-			return (begin, end)
-		return (begin - self.offset[0], end + self.offset[1])
-
-	def getOffsetBegin(self):
-		return self.offset[0]/60
-
-	def getOffsetEnd(self):
-		return self.offset[1]/60
-
-	def hasAfterEvent(self):
-		return len(self.afterevent)
-
-	def hasAfterEventTimespan(self):
-		for afterevent in self.afterevent:
-			if afterevent[1][0] is not None:
-				return True
+	def checkServices(self, service):
+		if len(self.services) or len(self.bouquets): 
+			return service not in self.getFullServices()
 		return False
 
-	def getAfterEventTimespan(self, end):
-		for afterevent in self.afterevent:
-			if not self.checkAnyTimespan(end, *afterevent[1]):
-				return afterevent[0]
-		return None
+	def checkTimespan(self, begin):
+		return self.checkAnyTimespan(begin, *self.timespan)
 
 	def getAfterEvent(self):
 		for afterevent in self.afterevent:
@@ -392,75 +435,72 @@ class AutoTimerComponent(object):
 				return afterevent[0]
 		return None
 
-	def getEnabled(self):
-		return self.enabled and "yes" or "no"
+	def getAfterEventTimespan(self, end):
+		for afterevent in self.afterevent:
+			if not self.checkAnyTimespan(end, *afterevent[1]):
+				return afterevent[0]
+		return None
 
-	def getJustplay(self):
-		return self.justplay and "1" or "0"
+### Misc
 
-	def hasDestination(self):
-		return self.destination is not None
-	
-	def getDestination(self):
-		return self.destination is not None
+	def __copy__(self):
+		return self.__class__(
+			self.id,
+			self.name,
+			self.match,
+			self.enabled,
+			timespan = self.timespan,
+			services = self.services,
+			offset = self.offset,
+			afterevent = self.afterevent,
+			exclude = (self.getExcludedTitle(), self.getExcludedShort(), self.getExcludedDescription(), self.getExcludedDays()),
+			maxduration = self.maxduration,
+			destination = self.destination,
+			include = (self.getIncludedTitle(), self.getIncludedShort(), self.getIncludedDescription(), self.getIncludedDays()),
+			matchCount = self.matchCount,
+			matchLeft = self.matchLeft,
+			matchLimit = self.matchLimit,
+			matchFormatString = self.matchFormatString,
+			lastBegin = self.lastBegin,
+			justplay = self.justplay,
+			avoidDuplicateDescription = self.avoidDuplicateDescription,
+			bouquets = self.bouquets,
+			tags = self.tags
+		)
 
-	def hasCounter(self):
-		return self.matchCount != 0
+	def __deepcopy__(self, memo):
+		return self.__class__(
+			self.id,
+			self.name,
+			self.match,
+			self.enabled,
+			timespan = self.timespan,
+			services = self.services[:],
+			offset = self.offset and self.offset[:],
+			afterevent = self.afterevent[:],
+			exclude = (self.getExcludedTitle(), self.getExcludedShort(), self.getExcludedDescription(), self.exclude[3][:]),
+			maxduration = self.maxduration,
+			destination = self.destination,
+			include = (self.getIncludedTitle(), self.getIncludedShort(), self.getIncludedDescription(), self.include[3][:]),
+			matchCount = self.matchCount,
+			matchLeft = self.matchLeft,
+			matchLimit = self.matchLimit,
+			matchFormatString = self.matchFormatString,
+			lastBegin = self.lastBegin,
+			justplay = self.justplay,
+			avoidDuplicateDescription = self.avoidDuplicateDescription,
+			bouquets = self.bouquets[:],
+			tags = self.tags[:]
+		)
 
-	def hasCounterFormatString(self):
-		return self.matchFormatString != ''
-
-	def getCounter(self):
-		return self.matchCount
-
-	def getCounterLeft(self):
-		return self.matchLeft
-
-	def getCounterLimit(self):
-		return self.matchLimit
-
-	def getCounterFormatString(self):
-		return self.matchFormatString
-
-	def checkCounter(self, timestamp):
-		# 0-Count is considered "unset"
-		if self.matchCount == 0:
+	def __eq__(self, other):
+		try:
+			return self.id == other.id
+		except AttributeError:
 			return False
 
-		# Check if event is in current timespan (we can only manage one!)
-		limit = strftime(self.matchFormatString, timestamp)
-		if limit != self.matchLimit:
-			return True
-
-		if self.matchLeft > 0:
-			self.matchLeft -= 1
-			return False
-		return True
-
-	def update(self, begin, timestamp):
-		# Only update limit when we have new begin
-		if begin > self.lastBegin:
-			self.lastBegin = begin
-
-			# Update Counter:
-			# %m is Month, %U is week (sunday), %W is week (monday)
-			newLimit = strftime(self.matchFormatString, timestamp)
-
-			if newLimit != self.matchLimit:
-				self.matchLeft = self.matchCount
-				self.matchLimit = newLimit
-
-	def getLastBegin(self):
-		return self.lastBegin
-
-	def getAvoidDuplicateDescription(self):
-		return self.avoidDuplicateDescription
-
-	def hasTags(self):
-		return len(self.tags) > 0
-	
-	def getTags(self):
-		return self.tags
+	def __ne__(self, other):
+		return not self.__eq__(other)
 
 	def __repr__(self):
 		return ''.join([
