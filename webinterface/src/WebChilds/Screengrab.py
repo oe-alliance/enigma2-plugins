fe80e24e82e5c8b637f73bbe0bb88b91e5dbc10b
Rico Schulte <ricoschulte@users.schwerkraft.elitedvb.net>
2008-08-23 11:17:22 +0000

    adding support for new commandline options of Seddis AiO Dreambox Screengrabber V0.8

--- webinterface/src/WebChilds/Screengrab.py
+++ webinterface/src/WebChilds/Screengrab.py
@@ -6,71 +6,120 @@ from os import path as os_path, remove as os_remove
 
 class GrabResource(resource.Resource):
     """
-        this is a interface to Seddis AiO Dreambox Screengrabber
+        this is a interface to Seddis AiO Dreambox Screengrabber 
+        version 0.8 or lower
     """
     grab_bin = "/usr/bin/grab"
-    grab_target = "/tmp/screenshot.bmp"
-
+    
     def render(self, req):
-        class GrabStream(stream.ProducerStream):
-            def __init__(self,cmd,target=None,save=False):
-                self.cmd = cmd
-                self.target = target
-                self.save = save
-                self.output = ""
-                stream.ProducerStream.__init__(self)
+        grab_command = self.grab_bin+" "
+        
+        #choosing what to grab
+        if req.args.has_key("command"):
+            # using 'command' for older versions
+            cmd = req.args['command'][0].replace("-","")
+            if cmd == "o":
+                #-o only grab osd (framebuffer)
+                grab_command += " -o "
+            elif cmd == "v":
+                #-v only grab video
+                grab_command += " -v "
+        elif req.args.has_key("o"):
+            grab_command += " -o "
+        elif req.args.has_key("v"):
+            grab_command += " -v " 
+                  
+        #chossing the imageformat
+        if req.args.has_key("format"):
+            formatraw = req.args["format"][0]
+            if formatraw == "png":
+                #-p produce png files instead of bmp
+                imageformat = "png"
+                grab_command += " -p "
+            elif formatraw == "jpg":
+                #-j (quality) produce jpg files instead of bmp (quality 0-100)
+                imageformat = "jpg"
+                if req.args.has_key("jpgquali"):
+                    grab_command += " -j %s "%req.args["jpgquali"][0]
+                else:    
+                    grab_command += " -j 80 "
+            else:
+                imageformat = "bmp"
+        else:
+            imageformat = "bmp"
 
-                self.container = eConsoleAppContainer()
-                self.container.appClosed.get().append(self.cmdFinished)
-                self.container.dataAvail.get().append(self.dataAvail)
-                self.container.execute(cmd)
+        
+        #-d always use osd resolution (good for skinshots)
+        if req.args.has_key("d"):
+            grab_command += " -d " 
 
-            def cmdFinished(self,data):
-                if int(data) is 11 and self.target is not None:
-                    try:
-                        fp = open(self.target)
-                        self.write(fp.read())
-                        fp.close()
-                        if self.save is False:
-                            os_remove(self.target)
-                    except Exception,e:
-                        self.write("internal error while reading target file")
-                elif int(data) is 0 and self.target is None:
-                    self.write(self.output)
-                elif int(data) is 1:
-                    self.write(self.output)
-                else:
-                    self.write("internal error")
-                self.finish()
+        #-n dont correct 16:9 aspect ratio
+        if req.args.has_key("n"):
+            grab_command += " -n " 
+            
+        #-r (size) resize to to a fixed width, maximum: 1920
+        if req.args.has_key("r"):
+            grab_command += " -r %s " % req.args['r'][0]
 
-            def dataAvail(self,data):
-                self.output += data
+        #-l always 4:3, create letterbox if 16:9
+        if req.args.has_key("l"):
+            grab_command += " -l "
 
+        #target filename
         if req.args.has_key("filename"):
             filetarget = req.args['filename'][0]
         else:
-            filetarget = self.grab_target
+            filetarget = "/tmp/screenshot."+imageformat
 
+        # choose if we leave the file in /tmp
         if req.args.has_key("save"):
             save_image = True
         else:
             save_image = False
 
-        headers = http_headers.Headers()
-        headers.addRawHeader('Content-Disposition', 'inline; filename=screenshot.bmp;')
-        headers.addRawHeader('Content-Type','image/bmp')
-
         if os_path.exists(self.grab_bin) is not True:
-            return    http.Response(responsecode.OK,stream="grab is not installed at '%s'. go and fix it."%self.grab_bin)
-        elif req.args.has_key("command"):
-            cmd = req.args['command'][0].replace("-","")
-            if cmd == "o":
-                return http.Response(responsecode.OK,headers,stream=GrabStream(self.grab_bin+" -o "+filetarget,target=filetarget,save=save_image))
-            elif cmd == "v":
-                return http.Response(responsecode.OK,headers,stream=GrabStream(self.grab_bin+" -v "+filetarget,target=filetarget,save=save_image))
-            elif cmd == "":
-                return http.Response(responsecode.OK,headers,stream=GrabStream(self.grab_bin+" "+filetarget,target=filetarget,save=save_image))
-            else:
-                return http.Response(responsecode.OK,headers,stream=GrabStream(self.grab_bin+" -h"))
+            return http.Response(responsecode.OK,stream="grab is not installed at '%s'. go and fix it."%self.grab_bin)
         else:
-            return http.Response(responsecode.OK,headers,stream=GrabStream(self.grab_bin+" "+filetarget,target=filetarget,save=save_image))
+            headers = http_headers.Headers()
+            headers.addRawHeader('Content-Disposition', 'inline; filename=screenshot.%s;'%imageformat)
+            headers.addRawHeader('Content-Type','image/%s'%imageformat)
+            return http.Response(responsecode.OK,headers,stream=GrabStream(grab_command+filetarget,target=filetarget,save=save_image))
+       
+class GrabStream(stream.ProducerStream):
+    """
+        used to start the grab-bin in the console in the background
+        while this takes some time, the browser must wait until the grabis finished
+    """
+    def __init__(self,cmd,target=None,save=False):
+        self.cmd = cmd
+        self.target = target
+        self.save = save
+        self.output = ""
+        stream.ProducerStream.__init__(self)
+
+        self.container = eConsoleAppContainer()
+        self.container.appClosed.get().append(self.cmdFinished)
+        self.container.dataAvail.get().append(self.dataAvail)
+        print "AiO grab starting aio grab with cmdline:",self.cmd
+        self.container.execute(cmd)
+
+    def cmdFinished(self,data):
+        if int(data) is 0 and self.target is not None:
+            try:
+                fp = open(self.target)
+                self.write(fp.read())
+                fp.close()
+                if self.save is False:
+                    os_remove(self.target)
+            except Exception,e:
+                self.write("internal error while reading target file")
+        elif int(data) is 0 and self.target is None:
+            self.write(self.output)
+        elif int(data) is 1:
+            self.write(self.output)
+        else:
+            self.write("internal error")
+        self.finish()
+
+    def dataAvail(self,data):
+        print "AiO grab:",data
