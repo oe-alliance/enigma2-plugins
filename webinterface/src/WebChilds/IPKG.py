c3374b5fbb7ac373e559ec2c73938b2a1e359e12
Stephan Reichholf <sreichholf@users.schwerkraft.elitedvb.net>
2009-01-05 00:05:53 +0000

    * much simpler and way safer code

--- webinterface/src/WebChilds/IPKG.py
+++ webinterface/src/WebChilds/IPKG.py
@@ -3,30 +3,66 @@ from enigma import eConsoleAppContainer
 from twisted.web2 import resource, stream, responsecode, http
 
 class IPKGResource(resource.Resource):
-    def render(self, req):
-       self.args = req.args
-       if self.getArg("command") == "update":
-           return self.doUpdate()
-       elif self.getArg("command") == "upgrade":
-           return self.doUpgrade()
-       elif self.getArg("command") == "list_installed":
-           return self.doListInstalled()
-       elif self.getArg("command") == "list":
-           return self.doListAvaiable()
-       elif self.getArg("command") == "search":
-           return self.doSearch()
-       elif self.getArg("command") == "info":
-           return self.doInfo()
-       elif self.getArg("command") == "status":
-           return self.doInfo()
-       elif self.getArg("command") == "install":
-           return self.doInstall()
-       elif self.getArg("command") == "remove":
-           return self.doRemove()
-       elif self.getArg("command"):
-           return self.doErrorPage("no or unknown command")
-       else:
-           return self.doIndexPage()
+    IPKG_PATH = "/usr/bin/ipkg"
+    
+    SIMPLECMDS = [ "list", "list_installed", "update", "upgrade" ]
+    PACKAGECMDS = [ "info", "status", "install", "remove" ]
+    FILECMDS = [ "search" ]
+    
+  
+    def render(self, request):
+        self.args = request.args
+        self.command = self.getArg("command")
+        
+        if self.command is not None:
+            
+            if self.command in IPKGResource.SIMPLECMDS:
+                return self.execSimpleCmd()
+            
+            elif self.command in IPKGResource.PACKAGECMDS:
+                return self.execPackageCmd() 
+            
+            elif self.command in IPKGResource.FILECMDS:
+                return self.execFileCmd()
+                
+            else:
+                return self.doErrorPage("Unkown command: ", self.command)
+        else:
+            return self.doIndexPage()
+            
+
+    def buildCmd(self, parms = []):
+        cmd = [IPKGResource.IPKG_PATH, "ipkg", self.command] + parms
+        print "[IPKG.py] cmd: %s" %cmd
+        return cmd
+
+    
+    def execCmd(self, parms = []):
+        cmd = self.buildCmd(parms)
+        return http.Response(responsecode.OK,stream=IPKGConsoleStream(cmd) )
+    
+    
+    def execSimpleCmd(self):
+         return self.execCmd()
+
+
+    def execPackageCmd(self):
+        package = self.getArg("package")
+        if package is not None:
+            return self.execCmd([package])
+        
+        else:
+            return self.doErrorPage("Missing parameter: package")
+        
+        
+    def execFileCmd(self):
+        file = self.getArg("file")
+        if file is not None:
+            return self.execCmd([file])
+        
+        else:
+            return self.doErrorPage("Missing parameter: file")
+        
 
     def doIndexPage(self):
         html  = "<html><body>"
@@ -43,70 +79,31 @@ class IPKGResource(resource.Resource):
         html += "</body></html>"
         return http.Response(responsecode.OK,stream=html)
 
-    def doUpdate(self):
-        return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg update"))
         
-    def doUpgrade(self):
-        return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg upgrade"))
-        
-    def doListInstalled(self):
-        return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg list_installed"))
-        
-    def doListAvaiable(self):
-        return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg list"))
-
-    def doSearch(self):
-        if self.getArg("file"):
-            return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg search "+self.getArg("file")))
-        else:
-            return self.doErrorPage("no file to search givven")
-
-    def doInfo(self):
-        if self.getArg("package"):
-            return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg info "+self.getArg("package")))
-        else:
-            return self.doErrorPage("no package to print info givven")
-
-    def doStatus(self):
-        if self.getArg("package"):
-            return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg status "+self.getArg("package")))
-        else:
-            return self.doErrorPage("no package to print status givven")
-
-    def doInstall(self):
-        if self.getArg("package"):
-            return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg install "+self.getArg("package")))
-        else:
-            return self.doErrorPage("no package to install givven")
-
-    def doRemove(self):
-        if self.getArg("package"):
-            return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg remove "+self.getArg("package")))
-        else:
-            return self.doErrorPage("no package to remove givven")
-
-    def doListAvaiable(self):
-        return http.Response(responsecode.OK,stream=IPKGConsoleStream("ipkg list"))
-        
-    def doErrorPage(self,errormsg):
+    def doErrorPage(self, errormsg):
         return http.Response(responsecode.OK,stream=errormsg)
     
-    def getArg(self,key):
+    
+    def getArg(self, key):
         if self.args.has_key(key):
             return self.args[key][0]
         else:
-            return False
+            return None
 
 class IPKGConsoleStream(stream.ProducerStream):
-    def __init__(self,cmd):
+    def __init__(self, cmd):
         stream.ProducerStream.__init__(self)
         self.container = eConsoleAppContainer()
-        self.container.appClosed.append(self.cmdFinished)
+        
         self.container.dataAvail.append(self.dataAvail)
-        self.container.execute(cmd)
+        self.container.appClosed.append(self.cmdFinished)
+        
+        self.container.execute(*cmd)
 
-    def cmdFinished(self,data):
+    
+    def cmdFinished(self, data):
         self.finish()    
             
-    def dataAvail(self,data):
+    
+    def dataAvail(self, data):
         self.write(data)
