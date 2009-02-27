ddab2b5c82c6aca4b3760812b748516a4e49332e
Stephan Reichholf <sreichholf@users.schwerkraft.elitedvb.net>
2009-02-27 17:12:38 +0000

    add list property

--- webinterface/src/WebComponents/Sources/Hdd.py
+++ webinterface/src/WebComponents/Sources/Hdd.py
@@ -8,11 +8,36 @@ class Hdd(Source):
 
 	def getHddData(self):
 		if len(harddiskmanager.hdd) > 0:
-			return harddiskmanager.hdd[0] # TODO, list more than the first harddisc if there are more than one. but this requires many changes in the way the webif generates the responses
+			return harddiskmanager.hdd[0]
 		else:
 			return None
 
 	hdd = property(getHddData)
+
+	def getList(self):
+		disks = []
+		for hdd in harddiskmanager.hdd:
+			model = "%s" %( hdd.model() )
+			capacity = "%s" %( hdd.capacity() )
+
+			if hdd.free() <= 1024:
+				free = "%i MB" %( hdd.free() )
+			else:
+				free = float(hdd.free()) / float(1024)
+				free = "%.3f GB" %free
+			disk = [model, capacity, free]
+			disks.append(disk)
+		
+		return disks
+		
+			
+	list = property(getList)
+	
+	lut = { "Model" : 0,
+			"Capacity" : 1,
+			"Free" : 2		
+		}
+
 
 	def destroy(self):
 		Source.destroy(self)
