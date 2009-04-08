94f0060a61d61de6bac9486684c5db78c91b43ec
Michael Alexejew <luke_s@users.schwerkraft.elitedvb.net>
2009-04-08 19:29:58 +0000

    dreamIRC initial check-in

---
+++ dreamirc/src/e2account.py
@@ -0,0 +1,78 @@
+# -*- Python -*-
+#
+# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# See LICENSE for details. 
+
+#
+import string
+import ircsupport
+import e2chat, dreamIRCTools, dreamIRCSetup
+
+
+class AccountManager:
+	"""I am responsible for managing a user's accounts.
+
+	That is, remembering what accounts are available, their settings,
+	adding and removal of accounts, etc.
+
+	@ivar accounts: A collection of available accounts.
+	@type accounts: mapping of strings to L{Account<interfaces.IAccount>}s.
+	"""
+	def __init__(self,session):
+		self.chatui = e2chat.ChatUI()
+		self.config = dreamIRCSetup.dreamIRCConfig()
+		self.accounts = self.config.load()
+#		self.startConnect()	
+
+#        if self.accounts==False:
+#            print "You have defined no accounts."
+#        else:
+#            for acct in self.accounts:
+#				acct.logOn(self.chatui)
+
+	def startConnect(self):
+		if self.accounts==False:
+			print "You have defined no accounts."
+		else:
+			for acct in self.accounts:
+				acct.logOn(self.chatui)
+
+	def getSnapShot(self):
+		"""A snapshot of all the accounts and their status.
+
+		@returns: A list of tuples, each of the form
+			(string:accountName, boolean:isOnline,
+			boolean:autoLogin, string:gatewayType)
+		"""
+		data = []
+#		for account in self.accounts.values():
+		for account in self.accounts:
+			data.append((account.accountName, account.isOnline(),account.autoLogin, account.gatewayType))
+		return data
+
+	def isEmpty(self):
+		return len(self.accounts) == 0
+
+	def getConnectionInfo(self):
+		connectioninfo = []
+		for account in self.accounts:
+			connectioninfo.append(account.isOnline())
+		return connectioninfo
+
+	def addAccount(self, account):
+		self.accounts[account.accountName] = account
+
+	def delAccount(self, accountName):
+		del self.accounts[accountName]
+
+	def connect(self, accountName, chatui):
+		"""
+		@returntype: Deferred L{interfaces.IClient}
+		"""
+		print "----1---- %s" % accountName
+		return self.accounts[accountName].logOn(chatui)
+
+	def quit(self):
+		pass
+		#for account in self.accounts.values():
+		#    account.logOff()  - not yet implemented
