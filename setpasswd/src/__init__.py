7ce039ceb8dd5646a4b908b7e6eb6ef4e1d994ec
Michael Alexejew <luke_s@users.schwerkraft.elitedvb.net>
2009-05-27 16:57:10 +0000

    fix copy and paste error

--- setpasswd/src/__init__.py
+++ setpasswd/src/__init__.py
@@ -9,13 +9,13 @@ PluginLanguagePath = "SystemPlugins/SetPasswd/po"
 def localeInit():
         lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
         os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-        print "[WirelessLan] set language to ", lang
+        print "[SetPasswd] set language to ", lang
         gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 def _(txt):
         t = gettext.dgettext(PluginLanguageDomain, txt)
         if t == txt:
-                print "[WirelessLan] fallback to default translation for", txt
+                print "[SetPasswd] fallback to default translation for", txt
                 t = gettext.gettext(txt)
         return t
 
