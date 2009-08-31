75243ce275f7119f163d86bfef23244893574e75
Stephan Reichholf <sreichholf@users.schwerkraft.elitedvb.net>
2009-08-31 18:21:53 +0000

    forgot to bump version here

--- webinterface/src/__init__.py
+++ webinterface/src/__init__.py
@@ -12,7 +12,7 @@ from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os,gettext
 
-__version__ = "1.5rc1"
+__version__ = "1.6rc1"
 
 PluginLanguageDomain = "WebInterface"
 PluginLanguagePath = "Extensions/WebInterface/locale"
