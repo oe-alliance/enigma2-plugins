18947bad94ae081446ad951e20a2e5bbf2143a08
Moritz Venn <ritzmo@users.schwerkraft.elitedvb.net>
2009-09-20 16:17:51 +0000

    cleanup

--- webinterface/src/WebChilds/Screenpage.py
+++ webinterface/src/WebChilds/Screenpage.py
@@ -2,7 +2,7 @@ from twisted.web import resource, http, server, static
 
 from Plugins.Extensions.WebInterface import webif
 
-import os
+from os import path as os_path
 
 """
 	define all files in /web to send no XML-HTTP-Headers here
@@ -23,54 +23,48 @@ TextHtmlHeaderFiles = ['wapremote.xml', 'stream.xml', ]
 NoExplicitHeaderFiles = ['getpid.xml', 'tvbrowser.xml', ]
 
 class ScreenPage(resource.Resource):
-	def __init__(self, session, path, addSlash = False):		
+	def __init__(self, session, path, addSlash = False):
 		resource.Resource.__init__(self)
-				
+
 		self.session = session
 		self.path = path
 		self.addSlash = addSlash
-		
-	def render(self, request):	
-		if os.path.isfile(self.path):	
-			
-# Set the Header according to what's requested								
-			if self.path.split("/")[-1] in AppTextHeaderFiles:				
-				request.setResponseCode(http.OK)
+
+	def render(self, request):
+		path = self.path
+		if os_path.isfile(path):
+			lastComponent = path.split('/')[-1]
+
+			# Set the Header according to what's requested
+			request.setResponseCode(http.OK)
+			if lastComponent in AppTextHeaderFiles:
 				request.setHeader('Content-Type', 'application/text')
-				
-			elif self.path.split("/")[-1] in TextHtmlHeaderFiles or (self.path.endswith(".html.xml") and self.path.split("/")[-1] != "updates.html.xml"):
-				request.setResponseCode(http.OK)
-				request.setHeader('Content-Type', 'text/html; charset=UTF-8')				
-																					
-			elif self.path.split("/")[-1] in NoExplicitHeaderFiles:
-				request.setResponseCode(http.OK)				
-				
-			else:
-				request.setResponseCode(http.OK)				
-				request.setHeader('Content-Type', 'application/xhtml+xml; charset=UTF-8')	
+			elif lastComponent in TextHtmlHeaderFiles or (path.endswith(".html.xml") and lastComponent != "updates.html.xml"):
+				request.setHeader('Content-Type', 'text/html; charset=UTF-8')
+			elif lastComponent not in NoExplicitHeaderFiles:
+				request.setHeader('Content-Type', 'application/xhtml+xml; charset=UTF-8')
 
 			# now go and write the Output
-			# request.finish() is called inside webif.py (requestFinish() which is called via renderPage())			
-			webif.renderPage(request, self.path, self.session) # login?		
-		
-		elif os.path.isdir(self.path) and self.addSlash is True:			
+			# request.finish() is called inside webif.py (requestFinish() which is called via renderPage())
+			webif.renderPage(request, path, self.session) # login?
+
+		elif os_path.isdir(path) and self.addSlash is True:
 			return self.getChild("/", request).render(request)
-			
+
 		else:
 			request.setResponseCode(http.NOT_FOUND)
 			request.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n')
 			request.write("<html><head><title>Enigma2 WebControl</title></head><body><h1>404 - Page not found</h1></body></html>")
 			request.finish()
-				
-		return server.NOT_DONE_YET
-				
 
-	def getChild(self, path, request):	
-		path = "%s/%s" %(self.path, path)
-		
-		if path[-1:] == "/":
+		return server.NOT_DONE_YET
+
+	def getChild(self, path, request):
+		path = "%s/%s" % (self.path, path)
+
+		if path[-1] == "/":
 			path += "index.html"
-			
+
 		path += ".xml"
 		return ScreenPage(self.session, path)
 
