adc6a396f9cd45b79a9fc55a09262ffce50e8b65
Moritz Venn <ritzmo@users.schwerkraft.elitedvb.net>
2009-10-05 15:45:53 +0000

    everthing should fit one page now

--- autotimer/src/AutoTimerImporter.py
+++ autotimer/src/AutoTimerImporter.py
@@ -96,7 +96,7 @@ class AutoTimerImportSelector(Screen):
 class AutoTimerImporter(Screen):
 	"""Import AutoTimer from Timer"""
 
-	skin = """<screen name="AutoTimerImporter" title="Import AutoTimer" position="center,center" size="565,280">
+	skin = """<screen name="AutoTimerImporter" title="Import AutoTimer" position="center,center" size="565,290">
 		<ePixmap position="0,0" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
 		<ePixmap position="140,0" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
 		<ePixmap position="280,0" size="140,40" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" />
@@ -105,7 +105,7 @@ class AutoTimerImporter(Screen):
 		<widget source="key_green" render="Label" position="140,0" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
 		<widget source="key_yellow" render="Label" position="280,0" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
 		<widget source="key_blue" render="Label" position="420,0" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget name="list" position="5,45" size="555,225" scrollbarMode="showOnDemand" />
+		<widget name="list" position="5,45" size="555,240" scrollbarMode="showOnDemand" />
 	</screen>"""
 
 	def __init__(self, session, autotimer, name, begin, end, disabled, sref, afterEvent, justplay, dirname, tags):
