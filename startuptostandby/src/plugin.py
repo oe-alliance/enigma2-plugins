287821bc0a618290546a9ea5dcb16f1a9b0286c4
Moritz Venn <ritzmo@users.schwerkraft.elitedvb.net>
2009-10-08 11:01:32 +0000

    get rid of global session, unmute after leaving standby

--- startuptostandby/src/plugin.py
+++ startuptostandby/src/plugin.py
@@ -8,14 +8,7 @@ from enigma import eTimer
 config.plugins.startuptostandby = ConfigSubsection()
 config.plugins.startuptostandby.enabled = ConfigEnableDisable(default = False)
 
-my_global_session = None
-
-def timeout():
-	print "[StartupToStandby] Timeout, Open Standby Screen"
-	my_global_session.open(Standby)
-
 timer = eTimer()
-timer.callback.append(timeout)
 
 def main(session, **kwargs):
 	print "[StartupToStandby] Open Config Screen"
@@ -25,9 +18,10 @@ def main(session, **kwargs):
 def sessionstart(reason, session = None):
 	print "[StartupToStandby] autostart"
 	if config.plugins.startuptostandby.enabled.value and reason == 0:
-		global my_global_session
-		my_global_session = session
-		my_global_session.open(Standby)
+		open_standby = lambda: setattr(session.open(Standby),'wasMuted', 0)
+		timer.callback.append(open_standby)
+
+		session.open(Standby)
 		# wait 10 seconds before setting standby again -
 		# bad hack so we do not only have a blank screen but are actually in standby...
 		print "[StartupToStandby] start timer..."
