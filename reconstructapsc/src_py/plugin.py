67a866a7b9979a3eb38cc52b189d0a91c8648ff6
Anders Holst <aholst@users.schwerkraft.elitedvb.net>
2009-12-26 14:59:31 +0000

    Fix last fix - global variable recons_path

--- reconstructapsc/src_py/plugin.py
+++ reconstructapsc/src_py/plugin.py
@@ -7,9 +7,10 @@ from Components.ActionMap import ActionMap
 from enigma import eTimer, eServiceCenter, iServiceInformation, eConsoleAppContainer
 from os import access, chmod, X_OK
 
+recons_path = "/usr/lib/enigma2/python/Plugins/Extensions/ReconstructApSc/bin/reconstruct_apsc"
+
 def main(session, service, **kwargs):
 	# Hack to make sure it is executable
-	recons_path = "/usr/lib/enigma2/python/Plugins/Extensions/ReconstructApSc/bin/reconstruct_apsc"
 	if not access(recons_path, X_OK):
 		chmod(recons_path, 493)
 	session.open(ReconstructApSc, service, **kwargs)
