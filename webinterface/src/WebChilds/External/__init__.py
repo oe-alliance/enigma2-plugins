106f16cce202f468a42e47427514a56dde4d1f7e
Stephan Reichholf <sreichholf@users.schwerkraft.elitedvb.net>
2010-01-15 21:24:18 +0000

    use SCOPE_PLUGINS

--- webinterface/src/WebChilds/External/__init__.py
+++ webinterface/src/WebChilds/External/__init__.py
@@ -1,8 +1,9 @@
 from os import listdir
 from os.path import abspath, splitext
-
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 def importExternalModules():
-	for file in listdir(abspath("/usr/lib/enigma2/python/Plugins/Extensions/WebInterface/WebChilds/External/")):
+	dir = abspath(resolveFilename(SCOPE_PLUGINS) + "Extensions/WebInterface/WebChilds/External/")
+	for file in listdir(dir):
 		module_name, ext = splitext(file) # Handles no-extension files, etc.
 
 		if ext == '.py' and module_name != "__init__":				
