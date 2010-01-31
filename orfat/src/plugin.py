64baabb73e8ae41cdbfb54b9f6903d9cdf67c047
Nabil Hanna <ali@users.schwerkraft.elitedvb.net>
2010-01-31 12:35:01 +0000

    do not show non more working ORF.at onDemand plugin

--- orfat/src/plugin.py
+++ orfat/src/plugin.py
@@ -8,13 +8,8 @@ from Plugins.Plugin import PluginDescriptor
 
 ####################################################
 
-def mainORFIPTV(session, **kwargs):
+def main(session, **kwargs):
 	session.open(ORFIPTVMain)
 
-def mainORFonDemand(session, **kwargs):
-	session.open(ORFonDemandMain)
-
 def Plugins(**kwargs):
-	return [
-		PluginDescriptor(name="ORF.at IPTV", description="IPTV-Sendungen von ORF.at anschauen", where=[PluginDescriptor.WHERE_EXTENSIONSMENU, PluginDescriptor.WHERE_PLUGINMENU], icon="orf.png", fnc=mainORFIPTV),
-		PluginDescriptor(name="ORF.at onDemand", description="onDemand-Sendungen von ORF.at anschauen", where=[PluginDescriptor.WHERE_EXTENSIONSMENU, PluginDescriptor.WHERE_PLUGINMENU], icon="orf_ondemand.png", fnc=mainORFonDemand)]
+	return PluginDescriptor(name="ORF.at IPTV", description="IPTV-Sendungen von ORF.at anschauen", where=[PluginDescriptor.WHERE_EXTENSIONSMENU, PluginDescriptor.WHERE_PLUGINMENU], icon="orf.png", fnc=main)
