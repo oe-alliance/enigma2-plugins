da7e0152563ef97eb83d718745b74dab179a60c4
Stephan Reichholf <sreichholf@users.schwerkraft.elitedvb.net>
2010-02-26 22:01:43 +0000

    Bump version (1.6.1 now) Bump Gears manifest revision Update About

--- webinterface/src/__init__.py
+++ webinterface/src/__init__.py
@@ -3,7 +3,7 @@ from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os,gettext
 
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 
 PluginLanguageDomain = "WebInterface"
 PluginLanguagePath = "Extensions/WebInterface/locale"
