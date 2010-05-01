c6d6001aa699152f9ab6abb0c41ec32b726853ef
Vali Jordan <vali@users.schwerkraft.elitedvb.net>
2010-05-01 06:33:26 +0000

    Ai.HD-Controler first upload into CVS.

---
+++ aihdcontrol/src/comp/vhdConvClockToText.py
@@ -0,0 +1,74 @@
+from Converter import Converter
+from time import localtime, strftime
+from Components.Element import cached
+
+class vhdConvClockToText(Converter, object):
+	DEFAULT = 0
+	WITH_SECONDS = 1
+	IN_MINUTES = 2
+	DATE = 3
+	FORMAT = 4
+	AS_LENGTH = 5
+	TIMESTAMP = 6
+	STUNDEN = 7
+	
+	# add: date, date as string, weekday, ... 
+	# (whatever you need!)
+	
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		if type == "WithSeconds":
+			self.type = self.WITH_SECONDS
+		elif type == "InMinutes":
+			self.type = self.IN_MINUTES
+		elif type == "InStunden":
+			self.type = self.STUNDEN
+		elif type == "Date":
+			self.type = self.DATE
+		elif type == "AsLength":
+			self.type = self.AS_LENGTH
+		elif type == "Timestamp":	
+			self.type = self.TIMESTAMP
+		elif str(type).find("Format") != -1:
+			self.type = self.FORMAT
+			self.fmt_string = type[7:]
+		else:
+			self.type = self.DEFAULT
+
+	@cached
+	def getText(self):
+		time = self.source.time
+		if time is None:
+			return ""
+
+		# handle durations
+		if self.type == self.IN_MINUTES:
+			return "%d min" % (time / 60)
+		elif self.type == self.AS_LENGTH:
+			return "%d:%02d" % (time / 60, time % 60)
+		elif self.type == self.TIMESTAMP:
+			return str(time)
+		elif self.type == self.STUNDEN:
+			return "%d:%02d" % (time / 3600, (time / 60) - ((time / 3600) * 60))
+		
+		t = localtime(time)
+		
+		if self.type == self.WITH_SECONDS:
+			return "%2d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec)
+		elif self.type == self.DEFAULT:
+			return "%02d:%02d" % (t.tm_hour, t.tm_min)
+		elif self.type == self.DATE:
+			return strftime("%A %B %d, %Y", t)
+		elif self.type == self.FORMAT:
+			spos = self.fmt_string.find('%')
+			if spos > 0:
+				s1 = self.fmt_string[:spos]
+				s2 = strftime(self.fmt_string[spos:], t)
+				return str(s1+s2)
+			else:
+				return strftime(self.fmt_string, t)
+		
+		else:
+			return "???"
+
+	text = property(getText)
