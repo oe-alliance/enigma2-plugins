eef73df03c7309510bf3bc9d0057f5a7c38f52a6
MiLo <milo-software@users.sourceforge.net>
2011-01-24 19:20:11 +0100

    webif

--- webinterface/src/WebComponents/Sources/Movie.py
+++ webinterface/src/WebComponents/Sources/Movie.py
@@ -67,10 +67,13 @@ class Movie(Source):
 		tag = self.cmd['tag']
 		tag = tag and tag.lower()
 		for (serviceref, info, begin, unknown) in self.movielist.list:
+			if serviceref.flags & eServiceReference.mustDescent:
+				# Skip subdirectories (TODO: Browse?)
+				continue
 			rtime = info.getInfo(serviceref, iServiceInformation.sTimeCreate)
 
 			if rtime > 0:
-				t = FuzzyTime(rtime)
+				t = FuzzyTime(rtime, inPast=True)
 				begin_string = t[0] + ", " + t[1]
 			else:
 				begin_string = "undefined"
