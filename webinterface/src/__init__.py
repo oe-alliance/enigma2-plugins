057927727e491d6f9c757431dc302a1bdb0ad805
sreichholf <stephan@reichholf.net>
2011-05-01 20:12:55 +0200

    bump webif version to 1.6.7

--- webinterface/src/__init__.py
+++ webinterface/src/__init__.py
@@ -3,7 +3,7 @@ from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os, gettext, sha
 
-__version__ = "1.6.6"
+__version__ = "1.6.7"
 
 PluginLanguageDomain = "WebInterface"
 PluginLanguagePath = "Extensions/WebInterface/locale"
