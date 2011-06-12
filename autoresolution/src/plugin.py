ddbd13990f4ee70ce68134b409488d516fc43e0e
nightmann <nightmann@newnigma2.to>
2011-06-12 12:28:23 +0200

    fix identiation error

--- autoresolution/src/plugin.py
+++ autoresolution/src/plugin.py
@@ -354,7 +354,7 @@ def autostart(reason, **kwargs):
 def startSetup(menuid):
 	if menuid != "system":
 		return [ ]
-	 return [(_("Autoresolution"), autoresSetup, "autores_setup", 45)]
+	return [(_("Autoresolution"), autoresSetup, "autores_setup", 45)]
 
 def autoresSetup(session, **kwargs):
 	autostart(reason=0, session=session)
