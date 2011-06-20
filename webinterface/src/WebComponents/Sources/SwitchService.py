950b62adf7c8f46749ea2176b8088ce8473d5962
sreichholf <stephan@reichholf.net>
2011-06-21 00:41:18 +0200

    show service name instead of serviceref after zapping (it was about time, wasn't it?)

--- webinterface/src/WebComponents/Sources/SwitchService.py
+++ webinterface/src/WebComponents/Sources/SwitchService.py
@@ -1,11 +1,13 @@
 from Components.Sources.Source import Source
+from Components.Converter import ServiceName
 from Components.config import config
-from enigma import eServiceReference
+from enigma import eServiceReference, iPlayableServicePtr
 
 class SwitchService(Source):
 	def __init__(self, session):
 		Source.__init__(self)
 		self.session = session
+		self.info = None
 		self.res = ( False, "Parameter sRef is missing" )
 	
 	def handleCommand(self, cmd):		
@@ -22,15 +24,34 @@ class SwitchService(Source):
 		"""		
 		if pc:
 			config.ParentalControl.configured.value = False
+			
 		if config.plugins.Webinterface.allowzapping.value:
-			eref= eServiceReference(cmd["sRef"])
+			eref = eServiceReference(cmd["sRef"])
+			
 			if cmd["title"] is not None:
 				eref.setName(cmd["title"])
-			self.session.nav.playService(eref)	
+			self.session.nav.playService(eref)
+				
 			if pc:
 				config.ParentalControl.configured.value = pc
 			
-			return ( True, "Active service switched to %s" %cmd["sRef"] )
+			name = cmd["sRef"]					
+			if cmd["title"] is None:
+				service = self.session.nav.getCurrentService()
+				info = None
+				if isinstance(service, iPlayableServicePtr):
+					info = service and service.info()
+					ref = None
+								
+				if info != None:
+					name = ref and info.getName(ref)
+					if name is None:
+						name = info.getName()
+					name.replace('\xc2\x86', '').replace('\xc2\x87', '')			
+			elif eref.getName() != "":
+				name = eref.getName()
+			
+			return ( True, "Active service is now '%s'" %name )
 		
 		else:
 			return ( False, "Zapping is disabled in WebInterface Configuration" )
