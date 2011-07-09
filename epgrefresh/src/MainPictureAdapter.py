15572c4c4cd191764ca8d1f55e2d4705f165fb33
Moritz Venn <moritz.venn@freaque.net>
2011-07-09 18:35:07 +0200

    epgrefresh: fix typo

--- epgrefresh/src/MainPictureAdapter.py
+++ epgrefresh/src/MainPictureAdapter.py
@@ -10,19 +10,19 @@ from Components.config import config
 class MainPictureAdapter:
 	backgroundCapable = True
 	def __init__(self, session):
-		self.session = session
+		self.navcore = session.nav
 
 	def prepare(self):
 		if config.plugins.epgrefresh.enablemessage.value:
 			Notifications.AddNotification(MessageBox, _("EPG refresh starts scanning channels."), type=MessageBox.TYPE_INFO, timeout=4)
-		self.previousService = session.nav.getCurrentlyPlayingServiceReference()
+		self.previousService = self.navcore.getCurrentlyPlayingServiceReference()
 		return True
 
 	def play(self, service):
 		print "[EPGRefresh.MainPictureAdapter.play]"
-		return self.session.nav.playService(service)
+		return self.navcore.playService(service)
 
 	def stop(self):
 		if self.previousService is not None or Screens.Standby.inStandby:
-			self.session.nav.playService(self.previousService)
+			self.navcore.playService(self.previousService)
 
