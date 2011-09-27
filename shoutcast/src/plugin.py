98348fc219ed30edc6cc04825dad7c2aa62eda03
MiLo <milo-software@users.sourceforge.net>
2011-09-27 19:48:38 +0200

    shoutcast: Add reconnect error handling (theparasol)
    Experimental feature...

--- shoutcast/src/plugin.py
+++ shoutcast/src/plugin.py
@@ -100,10 +100,20 @@ class Favorite:
 
 class myHTTPClientFactory(HTTPClientFactory):
 	def __init__(self, url, method='GET', postdata=None, headers=None,
-	agent="SHOUTcast", timeout=0, cookies=None,
-	followRedirect=1, lastModified=None, etag=None):
+			agent="SHOUTcast", timeout=0, cookies=None,
+			followRedirect=1, lastModified=None, etag=None):
 		HTTPClientFactory.__init__(self, url, method=method, postdata=postdata,
 		headers=headers, agent=agent, timeout=timeout, cookies=cookies, followRedirect=followRedirect)
+
+	def clientConnectionLost(self, connector, reason):
+		lostreason=("Connection was closed cleanly" in vars(reason))
+		if lostreason==None:
+			print"[SHOUTcast] Lost connection, reason: %s ,trying to reconnect!" %reason
+			connector.connect()
+
+	def clientConnectionFailed(self, connector, reason):
+		print"[SHOUTcast] connection failed, reason: %s,trying to reconnect!" %reason
+		connector.connect()
 
 def sendUrlCommand(url, contextFactory=None, timeout=60, *args, **kwargs):
 	scheme, host, port, path = client._parse(url)
