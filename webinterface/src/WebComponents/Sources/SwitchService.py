162c8cd8a3d5a83037d16626780f01b128448026
Moritz Venn <moritz.venn@freaque.net>
2011-11-05 23:36:38 +0100

    SwitchService.py: cleanup whitespaces

--- webinterface/src/WebComponents/Sources/SwitchService.py
+++ webinterface/src/WebComponents/Sources/SwitchService.py
@@ -10,10 +10,10 @@ class SwitchService(Source):
 		self.session = session
 		self.info = None
 		self.res = ( False, "Parameter sRef is missing" )
-	
-	def handleCommand(self, cmd):		
+
+	def handleCommand(self, cmd):
 		self.res = self.switchService(cmd)
-		
+
 	def switchService(self, cmd):
 		print "[SwitchService] ref=%s" % cmd["sRef"]
 		if config.plugins.Webinterface.allowzapping.value:
@@ -22,10 +22,10 @@ class SwitchService(Source):
 				if cmd["sRef"] != None:
 					pc = config.ParentalControl.configured.value
 					if pc:
-						config.ParentalControl.configured.value = False			
-					
+						config.ParentalControl.configured.value = False
+
 					eref = eServiceReference(cmd["sRef"])
-					
+
 					if cmd["title"] is not None:
 						eref.setName(cmd["title"])
 
@@ -48,29 +48,29 @@ class SwitchService(Source):
 
 					if pc:
 						config.ParentalControl.configured.value = pc
-					
-					name = cmd["sRef"]					
+
+					name = cmd["sRef"]
 					if cmd["title"] is None:
 						service = self.session.nav.getCurrentService()
 						info = None
 						if isinstance(service, iPlayableServicePtr):
 							info = service and service.info()
 							ref = None
-										
+
 						if info != None:
 							name = ref and info.getName(ref)
 							if name is None:
 								name = info.getName()
-							name.replace('\xc2\x86', '').replace('\xc2\x87', '')			
+							name.replace('\xc2\x86', '').replace('\xc2\x87', '')
 					elif eref.getName() != "":
 						name = eref.getName()
-					
+
 					return ( True, "Active service is now '%s'" %name )
 				else:
 					return ( False, "Obligatory Parameter 'sRef' is missing" )
 			else:
-				return ( False, "Cannot zap while device is in Standby" )			
+				return ( False, "Cannot zap while device is in Standby" )
 		else:
 			return ( False, "Zapping is disabled in WebInterface Configuration" )
-	
+
 	result = property(lambda self: self.res)
