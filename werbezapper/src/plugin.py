0f57ec6ba0f9922320f28cf0305db0a026651ba0
betonme <glaserfrank(at)gmail.com>
2011-11-15 00:09:06 +0100

    WerbeZapper: BugFix: Custom Input Crash One Button Solution for MQB: Press button to start monitoring and zap away ZapAway check will now be done 10 seconds after zap event Bugfix: Rezap to monitored service was recognized as zap away

--- werbezapper/src/plugin.py
+++ werbezapper/src/plugin.py
@@ -1,19 +1,41 @@
 # for localized messages
 from . import _
 
+# Config
+from Components.config import *
+
 # Plugin
 from Plugins.Plugin import PluginDescriptor
 
 zapperInstance = None
 
+# Config options
+config.werbezapper          = ConfigSubsection()
+config.werbezapper.duration = ConfigNumber(default = 5)
+
+
 # Mainfunction
 def main(session, servicelist, **kwargs):
-	# Create Instance if none present, show Dialog afterwards
+	# Create Instance if none present
 	global zapperInstance
 	if zapperInstance is None:
 		from WerbeZapper import WerbeZapper
 		zapperInstance = session.instantiateDialog( WerbeZapper, servicelist, cleanup )
+	# Show dialog
 	zapperInstance.showSelection()
+
+# Instant start / stop monitoring
+def startstop(session, servicelist, **kwargs):
+	# Create Instance if none present
+	global zapperInstance
+	if zapperInstance is None:
+		from WerbeZapper import WerbeZapper
+		zapperInstance = session.instantiateDialog( WerbeZapper, servicelist, cleanup )
+	# Start or stop monitoring
+	if not zapperInstance.monitor_timer.isActive():
+		zapperInstance.startMonitoring()
+	else:
+		zapperInstance.stopMonitoring()
 
 def cleanup():
 	global zapperInstance
@@ -29,6 +51,13 @@ def Plugins(**kwargs):
 			description = _("Automatically zaps back to current service after given Time"),
 			where = PluginDescriptor.WHERE_EXTENSIONSMENU,
 			fnc = main,
+			needsRestart = False,
+		),
+		PluginDescriptor(
+			name = "Werbezapper Start / Stop monitoring",
+			description = _("Start / Stop monitoring instantly"),
+			where = PluginDescriptor.WHERE_EXTENSIONSMENU,
+			fnc = startstop,
 			needsRestart = False,
 		)
 	]
