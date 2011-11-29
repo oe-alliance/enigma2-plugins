619e083d8cafaffe0c20ea634784b6a6c890d8a1
Moritz Venn <moritz.venn@freaque.net>
2011-11-29 17:07:40 +0100

    webif/MP: try to get default pattern from mp
    this should automatically adopt changes in the media player if the
    format list should ever change again so we don't have to try and keep up
    with it (which has failed before reliably :D).

--- webinterface/src/WebComponents/Sources/MP.py
+++ webinterface/src/WebComponents/Sources/MP.py
@@ -83,7 +83,12 @@ class MP(Source):
 			else:
 				return (("empty", True, "playlist"),)
 
-		matchingPattern = "(?i)^.*\.(mp2|mp3|ogg|ts|wav|wave|m3u|pls|e2pls|mpg|vob|avi|divx|m4v|mkv|mp4|m4a|dat|flac|mov|m2ts)" #MediaPlayer-Match
+		# try to extract current pattern from media player and use it over our hardcoded one as default
+		try:
+			matchingPattern = mp.filelist.matchingPattern
+		except Exception:
+			matchingPattern = "(?i)^.*\.(mp2|mp3|ogg|ts|wav|wave|m3u|pls|e2pls|mpg|vob|avi|divx|m4v|mkv|mp4|m4a|dat|flac|mov|m2ts)" #MediaPlayer-Match
+
 		useServiceRef = False
 		if param["types"] == "audio":
 			matchingPattern = "(?i)^.*\.(mp3|ogg|wav|wave|m3u|pls|e2pls)"
