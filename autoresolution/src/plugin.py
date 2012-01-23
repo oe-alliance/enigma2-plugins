7d064f8a86416eea5f7f1f169dd9a90ceacf1733
Andy Blackburn <andyblac@o2.co.uk>
2012-01-23 00:11:58 +0000

    AutoResolution: fix BSOD when change default resolution.
    Signed-off-by: Andy Blackburn <andyblac@o2.co.uk>

--- autoresolution/src/plugin.py
+++ autoresolution/src/plugin.py
@@ -119,7 +119,7 @@ class AutoRes(Screen):
 		if port_changed:
 			print "port changed to", configEntry.value
 			if port:
-				config.av.videomode[port].removeNotifier(self.defaultModeChanged)
+				config.av.videomode[port].notifiers.remove(self.defaultModeChanged)
 			port = config.av.videoport.value
 			if port in config.av.videomode:
 				config.av.videomode[port].addNotifier(self.defaultModeChanged)
