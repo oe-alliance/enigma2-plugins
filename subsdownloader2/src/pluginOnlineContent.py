48606f61da6a63a7012eddd0370154854753125f
root <root@dm8000.(none)>
2012-04-02 16:39:57 +0000

    modified:   subsdownloader2/CONTROL/control 	modified:   subsdownloader2/src/SourceCode/xbmc_subtitles/services/Napisyme/service.py 	modified:   subsdownloader2/src/about.nfo 	new file:   subsdownloader2/src/pic/buttons/HD/key_0.png 	modified:   subsdownloader2/src/plugin.py 	modified:   subsdownloader2/src/pluginOnlineContent.py

--- subsdownloader2/src/pluginOnlineContent.py
+++ subsdownloader2/src/pluginOnlineContent.py
@@ -12,8 +12,9 @@ from Components.Ipkg import IpkgComponent
 
 zlib_link = "http://subs-downloader.googlecode.com/files/libzen_0.4.22-0.0_mipsel.ipk"
 libmediainfo_link = "http://subs-downloader.googlecode.com/files/libmediainfo_0.7.50-0.0_mipsel.ipk"
-#flag_counter = ""
-flag_counter_url = "http://s10.flagcounter.com/count/gEB/bg_FFFFFF/txt_000000/border_CCCCCC/columns_9/maxflags_50/viewers_Plugin+Users/labels_1/pageviews_1/flags_0/"
+#flag_counter_url = "http://s10.flagcounter.com/count/gEB/bg_FFFFFF/txt_000000/border_CCCCCC/columns_9/maxflags_50/viewers_Plugin+Users/labels_1/pageviews_1/flags_0/"
+URL_text_file = "http://subs-downloader.googlecode.com/svn/commertial_banners.txt"
+Subtitle_Downloader_temp_dir = '/tmp/SubsDownloader_cache/'
 
 class IsNewVersionCheck(threading.Thread):
     def __init__(self):
@@ -110,11 +111,50 @@ class PluginIpkUpdate(Screen, IsNewVersionCheck):
 	    self.close(None)		
 		
 		
-def flagcounetr(CallBackFunction):
-    flag = urllib.urlopen(flag_counter_url,)
-    Subtitle_Downloader_temp_dir = '/tmp/SubsDownloader_cache/' # Sprawdzac czy sciezka jest taka sama jak w plugin.py
-    picture_file = open(Subtitle_Downloader_temp_dir+"plugin_users.png", "wb")
-    picture_file.write(flag.read())
-    flag.close()
-    picture_file.close()
-    CallBackFunction
+#def flagcounetr(CallBackFunction):
+#    flag = urllib.urlopen(flag_counter_url,)
+#    #Subtitle_Downloader_temp_dir = '/tmp/SubsDownloader_cache/' # Sprawdzac czy sciezka jest taka sama jak w plugin.py
+#    picture_file = open(Subtitle_Downloader_temp_dir+"plugin_users.png", "wb")
+#    picture_file.write(flag.read())
+#    flag.close()
+#    picture_file.close()
+#    CallBackFunction
+    
+class CommertialBannerDownload(threading.Thread):
+    def __init__(self):
+	threading.Thread.__init__(self)
+	      
+    def __download_picture_urls(self):
+	picture_links = []
+	try:
+	    URL_file = urllib.urlopen(URL_text_file)
+	    picture_links = URL_file.readlines()
+	    URL_file.close()
+	except:
+	    print "Failed to download picture URLS from online text file"
+	picture_URLS_dict = []
+	for x in picture_links:
+	    if x[0:3] == "\xef\xbb\xbf":
+		x = x[3:]
+		if x[-1:] == '\n':
+		    x = x[0:-1]
+	    picture_URLS_dict.append(x)
+	return picture_URLS_dict
+
+    def __download_pictures(self):
+	pictures_URLS = self.__download_picture_urls()
+	picture_counter = 0
+	for x in pictures_URLS:
+	    try:
+		flag = urllib.urlopen(x,)
+		picture_file = open((Subtitle_Downloader_temp_dir+"%s.png" % picture_counter), "wb")
+		picture_file.write(flag.read())
+		flag.close()
+		picture_file.close()
+		picture_counter = picture_counter+1
+	    except:
+		print "Failed to download picture no %i", picture_counter
+	return True
+     
+    def run(self):
+	return self.__download_pictures()
