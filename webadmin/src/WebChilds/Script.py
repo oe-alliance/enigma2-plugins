056be050532ce3e1da2775527b49704c84081aae
Emanuel Strobel <dreambox.development@yahoo.com>
2012-04-06 19:56:59 +0200

    Add new plugin webadmin

---
+++ webadmin/src/WebChilds/Script.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from PKG import PKGConsoleStream
+
+from twisted.web import server, resource, http
+
+class Script(resource.Resource):
+	
+	def render(self, request):
+		self.args = request.args
+		self.command = self.getArg("command")
+
+		if self.command is not None:
+			return self.execCmd(request)
+
+	def execCmd(self, request):
+		cmd = self.command.split("+")
+		cmd[0] = "/usr/script/" + cmd[0]
+		print "[Script] cmd: %s" % cmd
+		request.setResponseCode(http.OK)
+		PKGConsoleStream(request, cmd)
+
+		return server.NOT_DONE_YET
+
+	def getArg(self, key):
+		if key in self.args:
+			return self.args[key][0]
+		else:
+			return None
---
+++ webadmin/src/WebScreens/WebAdminScreen.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from Plugins.Extensions.WebInterface.WebScreens import WebScreen
+
+class WebAdminScreen(WebScreen):
+	def __init__(self, session, request):
+		WebScreen.__init__(self, session, request)
+		from Components.Sources.StaticText import StaticText
+		from Plugins.Extensions.WebAdmin.WebComponents.Sources.PkgList import PkgList
+		from Plugins.Extensions.WebAdmin.WebComponents.Sources.PkgConfList import PkgConfList
+		from Plugins.Extensions.WebAdmin.WebComponents.Sources.WebScriptList import WebScriptList
+		
+		self["PkgList"] = PkgList(session)
+		self["PkgListWap"] = PkgList(session, wap=True)
+		
+		self["PkgConfList"] = PkgConfList(session, func=PkgConfList.LIST)
+		
+		self["WebScriptList"] = WebScriptList(session, func=WebScriptList.LIST)
+		#self["PkgConfListWap"] = PkgConfList(session, wap=True)
+		self["SwitchFeed"] = PkgConfList(session, func=PkgConfList.SWITCH)
+		self["BoxMem"] = PkgConfList(session, func=PkgConfList.MEM)
+		
+		self["Install_txt"] = StaticText(_("install"))
+		self["Update_txt"] = StaticText(_("update"))
+		self["Delete_txt"] = StaticText(_("delete"))
+		
+		
