94db0df5d8e2ac7be4052813d9241aa41331020e
Shaderman <shaderman@dreambox-tools.info>
2012-05-10 21:43:20 +0200

    toolkit: removed NotifiablePluginBrowser. Sorry, this code breaks the plugin menu. It crashes due to a missing skin for NotifiablePluginBrowser. Even if the skin is set with self.skinName = PluginBrowser, OLED display is broken when navigating through the menu.

--- toolkit/src/plugin.py
+++ toolkit/src/plugin.py
@@ -1,14 +1,2 @@
-def autostart(reason, *args, **kwargs):
-	import NotifiablePluginBrowser
-	if reason == 1:
-		NotifiablePluginBrowser.uninstall()
-
 def Plugins(**kwargs):
-	from Plugins.Plugin import PluginDescriptor
-	return [
-		PluginDescriptor(
-			where=PluginDescriptor.WHERE_AUTOSTART,
-			fnc=autostart,
-			needsRestart=False,
-		),
-	]
+	return []
