af74da7502cb2531113d43afcf7569cc9a8ccfa3
Mladen Horvat <acid-burn@opendreambox.org>
2012-05-29 08:50:56 +0200

    ZDFMediathek: - removed strange mp4 playback capability check, we don't need this as all dreamboxes except dm7025 can playback mp4 files. - cleanup EOL markers and fixed a typo.

--- zdfmediathek/src/plugin.py
+++ zdfmediathek/src/plugin.py
@@ -8,7 +8,7 @@ from Components.MultiContent import MultiContentEntryText, MultiContentEntryPixm
 from Components.Pixmap import Pixmap
 from Components.PluginComponent import plugins
 from enigma import eListboxPythonMultiContent, ePicLoad, eServiceReference, eTimer, getDesktop, gFont
-from os import listdir, popen
+from os import listdir
 from Plugins.Plugin import PluginDescriptor
 from Screens.ChoiceBox import ChoiceBox
 from Screens.HelpMenu import HelpableScreen
@@ -44,14 +44,7 @@ deviceName = HardwareInfo().get_device_name()
 PLAY_MP4 = False
 
 if not deviceName.startswith("dm7025"):
-	try:
-		#FIXMEE add better check ! ? !
-		for line in popen("opkg info gst-plugin-rtsp").readlines():
-			if line.find("Version: ") != -1:
-				if line[9:] >= "0.10.23-r7.1":
-					PLAY_MP4 = True
-	except:
-		pass
+	PLAY_MP4 = True
 
 try:
 	from LT.LTStreamPlayer import streamplayer
@@ -807,7 +800,7 @@ class ZDFMediathek(Screen, HelpableScreen):
 					self.getPage(curr[0][0])
 			elif streamplayer:
 				if streamplayer.connected:
-					if streamplayer.caching or streamclient.streaming:
+					if streamplayer.caching or streamplayer.streaming:
 						self.playCachedFile()
 
 	def left(self):
