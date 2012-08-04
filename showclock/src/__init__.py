85fed78768cfbef8d9747ae37dbd926ae3ec0f10
JÃ¼rgen Strasdas <Software@Strasdas.de>
2012-08-04 09:49:07 +0200

    [ShowClock] V0.1.6 Simplify translation code: Setting the os LANGUAGE variable isn't needed anymore

--- showclock/src/__init__.py
+++ showclock/src/__init__.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-import os,gettext
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import gettext
  
 PluginLanguageDomain = "ShowClock"
 PluginLanguagePath = "Extensions/ShowClock/locale"
  
 def localeInit():
-    lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
-    os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-    #print "[WebInterface] set language to ", lang
     gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
  
 def _(txt):
