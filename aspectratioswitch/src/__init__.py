94e2c3255e08aadbd75937d7e984c558c4f79548
JÃ¼rgen Strasdas <Software@Strasdas.de>
2012-09-15 19:31:07 +0200

    [AspectRatioSwitch]Initial checkin

---
+++ aspectratioswitch/src/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import gettext
+ 
+PluginLanguageDomain = "AspectRatioSwitch"
+PluginLanguagePath = "SystemPlugins/AspectRatioSwitch/locale"
+ 
+def localeInit():
+    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+ 
+def _(txt):
+    t = gettext.dgettext(PluginLanguageDomain, txt)
+    if t == txt:
+        #print "[%s] fallback to default translation for %s" %(PluginLanguageDomain, txt)
+        t = gettext.gettext(txt)
+    return t
+ 
+localeInit()
+language.addCallback(localeInit)
