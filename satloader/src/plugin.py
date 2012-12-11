17c92fbeb7931e42f0330e4312de1c995a3ed5be
Ismail Demir <ismail.demir@gmx.at>
2012-12-11 22:37:11 +0100

    [satloader] prepare for transponder selection while adding selected transponder to satellites.xml

--- satloader/src/plugin.py
+++ satloader/src/plugin.py
@@ -26,7 +26,8 @@ class Satloader(Screen):
 				<widget name="key_yellow" position="404,4" size="190,32" valign="center" halign="center" zPosition="1" font="Regular;22" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
 				<widget name="key_blue" position="602,4" size="190,32" valign="center" halign="center" zPosition="1" font="Regular;22" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
 				<widget name="list" position="10,40" size="780,400" scrollbarMode="showOnDemand" />
-				<widget name="info" position="10,450" size="780,50" zPosition="10" font="Regular;22" valign="center" halign="center" />
+				<widget name="info" position="10,450" size="780,50" zPosition="1" font="Regular;22" valign="center" halign="center" />
+				<ePixmap position="730,458" size="50,35" zPosition="2" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Satloader/key_info.png" alphatest="on" />
 			</screen>"""
 	elif framewidth == 720:
 		skin = """
@@ -40,7 +41,8 @@ class Satloader(Screen):
 				<widget name="key_yellow" position="280,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
 				<widget name="key_blue" position="420,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
 				<widget name="list" position="10,50" size="540,350" scrollbarMode="showOnDemand" />
-				<widget name="info" position="10,410" size="540,40" zPosition="10" font="Regular;20" valign="center" halign="center" />
+				<widget name="info" position="10,410" size="540,40" zPosition="1" font="Regular;20" valign="center" halign="center" />
+				<ePixmap position="505,418" size="35,25" zPosition="2" pixmap="/usr/share/enigma2/skin_default/buttons/key_info.png" alphatest="on" />
 			</screen>"""
 
 	def __init__(self, session, url=None, path=None):
@@ -54,14 +56,15 @@ class Satloader(Screen):
 		self["key_green"] = Label(_("Install"))
 		self["key_yellow"] = Label(_("Satellites"))
 		self["key_blue"] = Label(_("Multi Sat"))
-		self["myActionMap"] = ActionMap(["OkCancelActions", "ColorActions"],
+		self["myActionMap"] = ActionMap(["OkCancelActions", "ColorActions", "EPGSelectActions"],
 		{
 			"ok": self.btnOK,
 			"cancel": self.btnRed,
 			"red": self.btnRed,
 			"green": self.btnGreen,
 			"yellow": self.btnYellow,
-			"blue": self.btnBlue
+			"blue": self.btnBlue,
+			"info": self.btnInfo
 		}, -1)
 
 		self.onLayoutFinish.append(self.btnYellow)
@@ -110,6 +113,15 @@ class Satloader(Screen):
 		self["info"].setText(_("Downloading satellites from server ..."))
 		downloadPage("http://satellites.satloader.net/transponder.tar.gz", "/tmp/transponder.tar.gz").addCallback(self.downloadListTPCallback).addErrback(self.downloadListError)
 
+	def btnInfo(self):
+		if self["list"].l.getCurrentSelection() is not None:
+			if self["list"].l.getCurrentSelection()[0][3] is not None:
+				name = self["list"].l.getCurrentSelection()[0][0]
+				url = "/tmp/transponder/"+self["list"].l.getCurrentSelection()[0][1]
+				self.session.open(SatloaderConfig, name, url)
+			else:
+				self.session.open(MessageBox, _("Only for Multi Sat"), MessageBox.TYPE_INFO)
+
 	def downloadListError(self, ret):
 		self.session.open(MessageBox, text = _("Downloading satellites failed!"), type = MessageBox.TYPE_ERROR)
 		self["info"].setText(_("Downloading satellites failed!"))
@@ -128,7 +140,7 @@ class Satloader(Screen):
 			os.system("tar -xzf /tmp/transponder.tar.gz -C/tmp/transponder")
 			os.system("rm -f /tmp/transponder.tar.gz")
 			self.list.clearList()
-			idx=0
+			idx = 0
 			f = open("/tmp/transponder/transponder.lst", "r")
 			for line in f.readlines():
 				m = line.split(";")
@@ -146,6 +158,89 @@ class Satloader(Screen):
 	def restart(self, ret):
 		if ret is True:
 			self.session.open(TryQuitMainloop, 3)
+
+
+class SatloaderConfig(Screen):
+	framewidth = getDesktop(0).size().width()
+	if framewidth == 1280:
+		skin = """
+			<screen position="240,130" size="800,510" title="Satloader Config" >
+				<ePixmap position="8,4" size="190,32" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Satloader/button_red_sm.png" alphatest="on" />
+				<ePixmap position="206,4" size="190,32" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Satloader/button_green_sm.png" alphatest="on" />
+				<ePixmap position="404,4" size="190,32" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Satloader/button_yellow_sm.png" alphatest="on" />
+				<ePixmap position="602,4" size="190,32" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Satloader/button_blue_sm.png" alphatest="on" />
+				<widget name="key_red" position="8,4" size="190,32" valign="center" halign="center" zPosition="1" font="Regular;22" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
+				<widget name="key_green" position="206,4" size="190,32" valign="center" halign="center" zPosition="1" font="Regular;22" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
+				<widget name="list" position="10,40" size="780,400" scrollbarMode="showOnDemand" />
+				<widget name="info" position="10,450" size="780,50" zPosition="10" font="Regular;22" valign="center" halign="center" />
+			</screen>"""
+	elif framewidth == 720:
+		skin = """
+			<screen position="center,center" size="560,460" title="Satloader Config" >
+				<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="skin_default/buttons/yellow.png" position="280,0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="skin_default/buttons/blue.png" position="420,0" size="140,40" alphatest="on" />
+				<widget name="key_red" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget name="key_green" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget name="list" position="10,50" size="540,350" scrollbarMode="showOnDemand" />
+				<widget name="info" position="10,410" size="540,40" zPosition="10" font="Regular;20" valign="center" halign="center" />
+			</screen>"""
+
+	def __init__(self, session, name, url):
+		Screen.__init__(self, session)
+		self.list = SatloaderList([])
+		self["list"] = self.list
+		self["info"] = Label()
+		self["key_red"] = Label(_("Cancel"))
+		self["key_green"] = Label(_("Save"))
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
+		{
+			"ok": self.btnOK,
+			"cancel": self.btnRed,
+			"red": self.btnRed,
+			"green": self.btnGreen
+		}, -1)
+
+		try:
+			f = open(url, "r")
+			self.list.clearList()
+			idx = 0
+			for line in f.readlines():
+				if "transponder" in line:
+					m = line.split("\"")
+					pol = ""
+					if m[5] == "0":
+						pol = "H"
+					elif m[5] == "1":
+						pol = "V"
+					elif m[5] == "2":
+						pol = "L"
+					elif m[5] == "3":
+						pol = "R"
+					
+					text = "TP:" + " " + str(idx+1).zfill(3) + "   " + _("Frequency:") + " " + str(m[1]).zfill(8)[:5] + " " + pol + "   " + _("Symbol Rate:") + " " + str(m[3]).zfill(8)[:5]
+					self.list.addSelection(text, url, idx, True)
+					idx += 1
+			f.close()
+
+		except Exception, e:
+			print "Error:", e
+			self["info"].setText(_("Parsing failed!")+"\n"+str(e))
+
+		self["info"].setText(name)
+
+	def btnRed(self):
+		print "\n[SatloaderConfig] cancel\n"
+		self.close(None)
+
+	def btnOK(self):
+		if self["list"].l.getCurrentSelection() is not None:
+			if self["list"].l.getCurrentSelection()[0][3] is not None:
+				self.list.toggleSelection()
+
+	def btnGreen(self):
+		self.session.open(MessageBox, _("Not yet implemented"), MessageBox.TYPE_INFO)
 
 
 class SatloaderList(MenuList):
