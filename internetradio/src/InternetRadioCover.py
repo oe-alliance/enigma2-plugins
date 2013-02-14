9f6b0be5355019573a23e3f8fd5cb94d27743ed8
Dr.Best <dr.best@dreambox-tools.info>
2013-02-14 20:08:03 +0100

    new possibility to add "cover" image in the skin (screen name="InternetRadioFullScreen") for cover show in fullscreen display mode (request from humsat)

--- internetradio/src/InternetRadioCover.py
+++ internetradio/src/InternetRadioCover.py
@@ -28,17 +28,30 @@ class InternetRadioCover(Pixmap):
 		self.picload = ePicLoad()
 		self.picload.PictureData.get().append(self.paintIconPixmapCB)
 		self.callback = callback
+		self.picloaded = False
+		self.showPic = False
 
 	def onShow(self):
 		Pixmap.onShow(self)
-		self.picload.setPara((self.instance.size().width(), self.instance.size().height(), 1, 1, False, 1, "#00000000"))
-
+		if self.instance.size().width() > 0:
+			self.picload.setPara((self.instance.size().width(), self.instance.size().height(), 1, 1, False, 1, "#00000000"))
+			self.showPic = True
 	def paintIconPixmapCB(self, picInfo=None):
 		ptr = self.picload.getData()
-		if ptr != None:
+		if self.showPic and ptr != None:
 			self.instance.setPixmap(ptr.__deref__())
+			self.picloaded = True
 		if self.callback is not None:
 			self.callback()
 
 	def updateIcon(self, filename):
-		self.picload.startDecode(filename)
+		self.picloaded = False
+		if self.showPic:
+			self.picload.startDecode(filename)
+
+	def setPicloaded(self, value):
+		self.picloaded = value
+		
+	def getPicloaded(self):
+		return self.picloaded
+		
