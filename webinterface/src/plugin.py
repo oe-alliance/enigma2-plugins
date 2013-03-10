7ea0122a6d6e380f814ad8844240e41fd1baa6e1
Andreas Oberritter <obi@opendreambox.org>
2013-03-10 22:41:25 +0100

    webinterface: improve portability in check_passwd()

--- webinterface/src/plugin.py
+++ webinterface/src/plugin.py
@@ -468,16 +468,17 @@ def check_passwd(name, passwd):
 	except:
 		return False
 
-	if cryptedpass:
-		#shadowed or not, that's the questions here
-		if cryptedpass == 'x' or cryptedpass == '*':
-			try:
-				cryptedpass = getspnam(name)[1]
-			except:
-				return False
+	#shadowed or not, that's the questions here
+	if cryptedpass == 'x' or cryptedpass == '*':
+		try:
+			cryptedpass = getspnam(name)[1]
+		except:
+			return False
 
-		return crypt(passwd, cryptedpass) == cryptedpass
-	return False
+	if cryptedpass == '':
+		return True
+
+	return crypt(passwd, cryptedpass) == cryptedpass
 
 global_session = None
 
