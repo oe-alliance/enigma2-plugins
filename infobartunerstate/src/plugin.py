71b84e894b0087abc67982751f7ca6acb056d362
betonme <glaserfrank(at)gmail.com>
2013-12-02 16:55:04 +0100

    InfoBarTunerState: Bugfix: Position of IBTS if list goes up is enabled Sort order is now always the begin time

--- infobartunerstate/src/plugin.py
+++ infobartunerstate/src/plugin.py
@@ -37,7 +37,7 @@ from InfoBarTunerState import InfoBarTunerState, TunerStateInfo
 NAME = _("InfoBarTunerState")
 IBTSSHOW = _("Show InfoBarTunerState")
 IBTSSETUP = _("InfoBarTunerState Setup")
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 SUPPORT = "http://bit.ly/ibtsihad"
 DONATE = "http://bit.ly/ibtspaypal"
 ABOUT = "\n  " + NAME + " " + VERSION + "\n\n" \
