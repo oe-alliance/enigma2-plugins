8dfe26a845dd0258588a5df7408a72a57ae691c5
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2014-12-23 17:18:20 +0100

    Fix inverse logic default
    The default was to occupy a tuner during prime time .... huh?
    
    Some people do not even have dual tuners and even from those I get lots of feedback like "I can't record two programs at the same time", so do the EPG Refresh outside of prime time by default to have all tuners ready for prime time.

--- epgrefresh/src/plugin.py
+++ epgrefresh/src/plugin.py
@@ -18,11 +18,11 @@ from sys import stdout
 from time import time, localtime, mktime
 now = localtime()
 begin = mktime((
-	now.tm_year, now.tm_mon, now.tm_mday, 20, 15, \
+	now.tm_year, now.tm_mon, now.tm_mday, 07, 30, \
 	0, now.tm_wday, now.tm_yday, now.tm_isdst)
 )
 end = mktime((
-	now.tm_year, now.tm_mon, now.tm_mday, 06, 30, \
+	now.tm_year, now.tm_mon, now.tm_mday, 20, 00, \
 	0, now.tm_wday, now.tm_yday, now.tm_isdst)
 )
 
