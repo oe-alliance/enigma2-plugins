0d6c71b45c35ac022f7361f231e24c70fc57742a
rossi2000 <rossi_2000@hotmail.com>
2015-06-14 22:00:41 +0100

    [EPGRefresh] add to openvix epg menu aswell.

--- epgrefresh/src/plugin.py
+++ epgrefresh/src/plugin.py
@@ -265,7 +265,7 @@ config.plugins.epgrefresh.show_in_extensionsmenu.addNotifier(housekeepingExtensi
 config.plugins.epgrefresh.show_run_in_extensionsmenu.addNotifier(housekeepingExtensionsmenu, initial_call = False, immediate_feedback = True)
 
 def menu_main(menuid, **kwargs):
-	if getImageDistro() in ("openatv"):
+	if getImageDistro() in ("openvix", "openatv"):
 		if menuid != "epg":
 			return []
 	else:
