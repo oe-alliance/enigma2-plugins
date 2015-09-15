30239538ade0af1304e3d072872aaa0c7d171618
Andrew Blackburn <andyblac@icloud.com>
2015-09-15 13:25:40 +0100

    Revert "[AutoTimer] Fix last commit"
    This reverts commit a1461a1828f2783490ddad90d5f4cc41c78aaa7d.

--- autotimer/src/AutoTimer.py
+++ autotimer/src/AutoTimer.py
@@ -253,7 +253,6 @@ class AutoTimer:
 		# Precompute timer destination dir
 		dest = timer.destination or config.usage.default_path.value
 
-		match = timer.match
 		if timer.searchType == "description":
 			epgmatches = []
 			mask = (eServiceReference.isMarker | eServiceReference.isDirectory)
