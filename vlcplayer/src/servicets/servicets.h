440c457d13e6f5872cc853f8901aa746483af392
Betacentauri <betacentauri@arcor.de>
2017-02-26 16:03:49 +0100

    Adapt according to pli changes in E2 (again)

--- vlcplayer/src/servicets/servicets.h
+++ vlcplayer/src/servicets/servicets.h
@@ -66,6 +66,7 @@ public:
 
 	// not implemented
 	RESULT setTarget(int target) { return -1; };
+	RESULT setTarget(int target, bool noaudio = false) { return -1; };
 	RESULT setSlowMotion(int ratio) { return -1; };
 	RESULT setFastForward(int ratio) { return -1; };
 	RESULT audioChannel(ePtr<iAudioChannelSelection> &ptr) { ptr = this; return 0; };
