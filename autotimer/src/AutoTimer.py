386fbb15ec08e2bdcaca30602bf6683f288d6c77
iPkay <iPKay@gmx.com>
2019-03-06 18:42:39 +0100

    [autotimer] update (#343)
    * [autotimer] show bouquets in timerlist
    
    * [autotimer] fix for skipped autotimer with same name
    
    * [autotimer] add option for double timer on different services
    
    * [autotimer] version increased

--- autotimer/src/AutoTimer.py
+++ autotimer/src/AutoTimer.py
@@ -304,12 +304,15 @@ class AutoTimer:
 	def createTask(self):
 		self.timer_count = 0
 		self.completed = []
+		self.searchtimer = []
 		job = Components.Task.Job(_("AutoTimer"))
 		timer = None
 
 		# Iterate Timer
 		for timer in self.getEnabledTimerList():
-			task = Components.Task.PythonTask(job, timer.name)
+			taskname = timer.name + '_%d' %self.timer_count
+			task = Components.Task.PythonTask(job, taskname)
+			self.searchtimer.append((timer, taskname))
 			task.work = self.JobStart
 			task.weighting = 1
 			self.timer_count += 1
@@ -322,16 +325,19 @@ class AutoTimer:
 		return job
 
 	def JobStart(self):
-		for timer in self.getEnabledTimerList():
-			if timer.name not in self.completed:
-				self.parseTimer(timer, self.epgcache, self.serviceHandler, self.recordHandler, self.checkEvtLimit, self.evtLimit, self.autotimers, self.conflicting, self.similars, self.skipped, self.existing, self.timerdict, self.moviedict, self.simulateOnly)
+		for timer, taskname in self.searchtimer:
+			if taskname not in self.completed:
+				self.parseTimer(timer, self.epgcache, self.serviceHandler, self.recordHandler, self.checkEvtLimit, self.evtLimit, self.autotimers, self.conflicting, self.similars, self.skipped, self.existing, self.timerdict, self.moviedict, taskname, self.simulateOnly)
 				self.new += self.result[0]
 				self.modified += self.result[1]
 				break
 
-	def parseTimer(self, timer, epgcache, serviceHandler, recordHandler, checkEvtLimit, evtLimit, timers, conflicting, similars, skipped, existing, timerdict, moviedict, simulateOnly=False):
+	def parseTimer(self, timer, epgcache, serviceHandler, recordHandler, checkEvtLimit, evtLimit, timers, conflicting, similars, skipped, existing, timerdict, moviedict, taskname, simulateOnly=False):
 		new = 0
 		modified = 0
+
+		# enable multiple timer if services or bouquets specified (eg. recording the same event on sd service and hd service)
+		enable_multiple_timer = ((timer.services and 's' in config.plugins.autotimer.enable_multiple_timer.value or False) or (timer.bouquets and 'b' in config.plugins.autotimer.enable_multiple_timer.value or False))
 
 		# Precompute timer destination dir
 		dest = timer.destination or config.usage.default_path.value
@@ -582,7 +588,7 @@ class AutoTimer:
 				# We want to search for possible doubles
 				for rtimer in chain.from_iterable( itervalues(timerdict) ):
 					if not rtimer.disabled:
-						if self.checkDoubleTimers(timer, name, rtimer.name, begin, rtimer.begin, end, rtimer.end ):
+						if self.checkDoubleTimers(timer, name, rtimer.name, begin, rtimer.begin, end, rtimer.end, serviceref, str(rtimer.service_ref), enable_multiple_timer ):
 							oldExists = True
 							print("[AutoTimer] We found a timer with same StartTime, skipping event")
 							break
@@ -728,7 +734,7 @@ class AutoTimer:
 						# We might want to do the sanity check locally so we don't run it twice - but I consider this workaround a hack anyway
 						conflicts = recordHandler.record(newEntry)
 		self.result=(new, modified)
-		self.completed.append(timer.name)
+		self.completed.append(taskname)
 		sleep(0.5)
 
 	def JobMessage(self):
@@ -852,8 +858,9 @@ class AutoTimer:
 				retValue = True
 		return retValue
 
-	def checkDoubleTimers(self, timer, name1, name2, starttime1, starttime2, endtime1, endtime2):
+	def checkDoubleTimers(self, timer, name1, name2, starttime1, starttime2, endtime1, endtime2, serviceref1, serviceref2, multiple):
 		foundTitle = name1 == name2
 		foundstart = starttime1 == starttime2
 		foundend = endtime1 == endtime2
-		return foundTitle and foundstart and foundend
+		foundref = serviceref1 == serviceref2
+		return foundTitle and foundstart and foundend and (foundref or not multiple)
