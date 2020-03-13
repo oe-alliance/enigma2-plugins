c594cea61d4c3e0b254af2b6041dfb337ec127fc
prl <prl@ozemail.com.au>
2020-03-13 12:41:15 +1100

    Bump version number to 1.9

--- plugin.py
+++ plugin.py
@@ -7,7 +7,7 @@ an option to do the processing automatically in the background.
 Mike Griffin  8/02/2015
 '''
 
-__version__ = "1.8"
+__version__ = "1.9"
 
 from Plugins.Plugin import PluginDescriptor
 from Screens.MovieSelection import MovieSelection
