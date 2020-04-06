b4bb55ff89146a5febfc0ffebc846d0c40bd84b3
captain <captain.onboard@web.de>
2020-04-06 17:23:39 +0200

    [remotetimer] fix crash  AttributeError: E2Timer instance has no attribute 'ice_timer_id'

--- remotetimer/src/plugin.py
+++ remotetimer/src/plugin.py
@@ -199,7 +199,7 @@ class E2Timer:
 	def __init__(self, sref = "", sname = "", name = "", disabled = 0, failed = 0, \
 			timebegin = 0, timeend = 0, duration = 0, startprepare = 0, \
 			state = 0, repeated = 0, justplay = 0, eventId = 0, afterevent = 0, \
-			dirname = "", description = "", isAutoTimer = 0):
+			dirname = "", description = "", isAutoTimer = 0, ice_timer_id = None):
 		self.service_ref = RemoteService(sref, sname)
 		self.name = name
 		self.disabled = disabled
@@ -216,6 +216,7 @@ class E2Timer:
 		self.dirname = dirname
 		self.description = description
 		self.isAutoTimer = isAutoTimer
+		self.ice_timer_id = ice_timer_id
 
 class RemoteTimerSetup(Screen, ConfigListScreen):
 	skin = """
