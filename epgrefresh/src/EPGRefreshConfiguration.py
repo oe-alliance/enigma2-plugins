820362374d3fe58b9827e526e3ba8301ed9243a5
Huevos <rowland@moralet.com>
2021-03-22 23:44:29 +0100

    [EPGRefreshConfiguration] OpenViX: add list refresh on keySelect

--- epgrefresh/src/EPGRefreshConfiguration.py
+++ epgrefresh/src/EPGRefreshConfiguration.py
@@ -20,7 +20,7 @@ from Screens.HelpMenu import HelpMenu, HelpableScreen
 from Components.Sources.StaticText import StaticText
 
 # Configuration
-from Components.config import config, getConfigListEntry, configfile, NoSave
+from Components.config import config, getConfigListEntry, configfile, NoSave, ConfigBoolean, ConfigSelection
 from Screens.FixedMenu import FixedMenu
 from Tools.BoundFunction import boundFunction
 
@@ -373,4 +373,7 @@ class EPGRefreshConfiguration(Screen, HelpableScreen, ConfigListScreen):
 		
 		self.close(self.session, self.needsEnigmaRestart)
 
-
+	def keySelect(self): # used by OpenViX
+		ConfigListScreen.keySelect(self)
+		if self["config"].getCurrent() and isinstance(self["config"].getCurrent()[1], (ConfigBoolean, ConfigSelection)):
+			self._getConfig()
