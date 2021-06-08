39219a71f36a4af5bab4d447fe806d5d5b3bab02
Jörg Bleyel <jbleyel@gmx.net>
2021-06-08 11:17:53 +0200

    [AutoTimer] improve AutoTimerTestResource

--- autotimer/src/AutoTimerResource.py
+++ autotimer/src/AutoTimerResource.py
@@ -104,9 +104,9 @@ class AutoTimerTestResource(AutoTimerBaseResource):
 		id = get("id")
 		if id:
 			id = int(id[0])
-			autotimer.parseEPG(simulateOnly=True, uniqueId=id, callback=self.parsecallback)
-		else:
-			autotimer.parseEPG(simulateOnly=True, callback=self.parsecallback)
+		
+		autotimer.parseEPG(simulateOnly=True, uniqueId=id, callback=self.parsecallback)
+
 		return server.NOT_DONE_YET
 
 	def renderBackground(self, req, timers, skipped):
