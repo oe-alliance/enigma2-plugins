fd537ffd296aa1456ba493ae54a3942fc785f0cc
Brian Gregory <bdgregory@gmail.com>
2021-08-06 20:23:30 +0100

    Update plugin.py

--- autoresolution/src/plugin.py
+++ autoresolution/src/plugin.py
@@ -648,7 +648,6 @@ class AutoFrameRate(Screen):
 		self.init = False
 
 	def AutoVideoFramerateChanged(self):
-		print "[AutoFrameRate] got event evFramerateChanged"
 		if usable and config.plugins.autoresolution.mode.value == "auto":
 			if config.av.videoport.value in config.av.videomode:
 				if config.av.videomode[config.av.videoport.value].value in config.av.videorate:
