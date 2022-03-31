8734f15b892e0d549f3d4d43263a16e59fc68a0e
Huevos <rowland@moralet.com>
2022-03-31 18:00:34 +0200

    [StartupToStandbyConfiguration] Inherit from Setup.py

--- startuptostandby/src/StartupToStandbyConfiguration.py
+++ startuptostandby/src/StartupToStandbyConfiguration.py
@@ -1,67 +1,14 @@
-# GUI (Screens)
-from Screens.Screen import Screen
-from Components.ConfigList import ConfigListScreen
-
-# GUI (Summary)
-from Screens.Setup import SetupSummary
-
-# GUI (Components)
-from Components.ActionMap import ActionMap
-from Components.Sources.StaticText import StaticText
-
-# Configuration
+from Screens.Setup import Setup
 from Components.config import config, getConfigListEntry
 
 
-class StartupToStandbyConfiguration(Screen, ConfigListScreen):
-	"""Configuration of Startup To Standby"""
-
+class StartupToStandbyConfiguration(Setup):
 	def __init__(self, session):
-		Screen.__init__(self, session)
+		Setup.__init__(self, session=session, setup=None)
+		self.title = _("StartupToStandby Configuration")
 		self.skinName = ["StartupToStandbyConfiguration", "Setup"]
 
-		# Summary
-		self.setup_title = _("StartupToStandby Configuration")
-		self.onChangedEntry = []
-
-		# Buttons
-		self["key_red"] = StaticText(_("Cancel"))
-		self["key_green"] = StaticText(_("OK"))
-
-		# Define Actions
-		self["actions"] = ActionMap(["SetupActions"],
-			{
-				"cancel": self.keyCancel,
-				"save": self.keySave,
-			}
-		)
-
-		ConfigListScreen.__init__(
-			self,
-			[
-				getConfigListEntry(_("Enabled"), config.plugins.startuptostandby.enabled)
-			],
-			session=session,
-			on_change=self.changed
-		)
-
-		# Trigger change
-		self.changed()
-
-		self.onLayoutFinish.append(self.layoutFinished)
-
-	def layoutFinished(self):
-		self.setTitle(self.setup_title)
-
-	def changed(self):
-		for x in self.onChangedEntry:
-			x()
-
-	def getCurrentEntry(self):
-		return self["config"].getCurrent()[0]
-
-	def getCurrentValue(self):
-		return str(self["config"].getCurrent()[1].getText())
-
-	def createSummary(self):
-		return SetupSummary
+	def createSetup(self):
+		if not self.list: # only load once as is a single item
+			self.list = [getConfigListEntry(_("Enable"), config.plugins.startuptostandby.enabled)]
+			self["config"].list = self.list
