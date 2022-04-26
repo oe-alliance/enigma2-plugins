e90914565db7177590196b4987deb563cbd65ae3
Huevos <rowland@moralet.com>
2022-04-27 01:45:56 +0200

    [AutoTimer] make adding streams optional

--- autotimer/src/AutoTimer.py
+++ autotimer/src/AutoTimer.py
@@ -350,6 +350,9 @@ class AutoTimer:
 		def removeDuplicates(lst):
 			return [t for t in (set(tuple(i) for i in lst))]
 
+		def removeStreams(lst):
+			return [x for x in lst if "http" not in x[0]]
+
 		def getNonSearchableEvents(servicelist):
 			servicelist.insert(0, 'RITBDSE')
 			try:
@@ -457,6 +460,10 @@ class AutoTimer:
 
 		# Remove duplicates
 		epgmatches = removeDuplicates(epgmatches)
+		
+		# Remove streams
+		if not config.plugins.autotimer.include_streams.value:
+			epgmatches = removeStreams(epgmatches)
 
 		# Sort list of tuples by begin time 'B'
 		epgmatches.sort(key=itemgetter(3))
