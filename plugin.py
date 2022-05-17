e95ab482238d33cfdafb9b45b0f18b1e14517add
prl <prl@ozemail.com.au>
2022-05-17 10:37:28 +1000

    Ask user when cancelling Series2FolderConfig when there are unsaved changes
    EXIT and RED Cancel would exit Series2FolderConfig immediately even if there
    were unsaved changes.
    
    [plugin]
    
    Remove Series2FolderConfig.cancel() and Series2FolderConfig.save() and
    change Series2FolderConfig's ActionMap to use ConfigListScreen.keyCancel()
    and ConfigListScreen.keySave() instead.
    
    Using keyCancel() correctly asks the user if they want to cancel when there are
    unsaved changes. Using keySave() removes some redundant code.
    
    [plugin, control]
    
    Bump version number to 1.11dev7.

--- plugin.py
+++ plugin.py
@@ -9,7 +9,7 @@ an option to do the processing automatically in the background.
 Mike Griffin  8/02/2015
 '''
 
-__version__ = "1.11dev6"
+__version__ = "1.11dev7"
 
 from Plugins.Plugin import PluginDescriptor
 from Screens.MovieSelection import MovieSelection
@@ -719,9 +719,9 @@ class Series2FolderConfig(ConfigListScreen, Screen):
         ConfigListScreen.__init__(self, self.list, session)
 
         self["Conf"] = ActionMap(contexts=["SetupActions", "ColorActions"], actions={
-            "cancel": self.cancel,
-            "red": self.cancel,
-            "green": self.save,
+            "cancel": self.keyCancel,
+            "red": self.keyCancel,
+            "green": self.keySave,
             "blue": self.keyboard,
             "ok": self.keyboard,
         }, prio=-2)
@@ -786,12 +786,3 @@ class Series2FolderConfig(ConfigListScreen, Screen):
         selection = self["config"].getCurrent()
         if isinstance(selection[1], ConfigText):
             self.KeyText()
-
-    def cancel(self):
-        for x in self["config"].list:
-            x[1].cancel()
-        self.close(False)
-
-    def save(self):
-        self.saveAll()
-        self.close()
