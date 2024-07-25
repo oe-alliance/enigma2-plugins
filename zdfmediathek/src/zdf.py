ffc2edf3f131d308ff0e5d4d3d19d8f6498ad739
Mr.Servo <mrservo@gmx.de>
2024-07-25 10:50:51 +0200

    ZDF Mediathek update API-url

--- zdfmediathek/src/zdf.py
+++ zdfmediathek/src/zdf.py
@@ -36,7 +36,7 @@ FONT = "/usr/share/fonts/LiberationSans-Regular.ttf"
 if not path.exists(FONT):
     FONT = "/usr/share/fonts/nmsbd.ttf"
 addFont(FONT, "SRegular", 100, False)
-API_URL = "https://zdf-cdn.live.cellular.de/mediathekV2/"
+API_URL = "https://zdf-prod-futura.zdf.de/mediathekV2/"
 ColorList = [("default,#1a104485,#3D104485,#1aC0C0C0", "Trans-BrightBlue"), ("default,#050a1232,#1502050e,#05192d7c", "Trans-DarkBlue"), ("default,#05000000,#15000000,#606060", "Trans-BlackGray"), ("default,#05000000,#15000000,#ffff00", "Trans-BlackYellow"), ("default,#1a746962,#1502050e,#1a746962", "Trans-BrownBlue"), ("MiniTV,#104485,#0c366a,#C0C0C0", "BrightBlue MiniTV"), ("MiniTV,#0a1232,#02050e,#192d7c", "DarkBlue MiniTV"), ("MiniTV,#000000,#080808,#606060", "BlackGray MiniTV"), ("MiniTV,#000000,#080808,#ffff00", "BlackYellow MiniTV"), ("MiniTV,#746962,#02050e,#746962", "BrownBlue MiniTV")]
 config.plugins.ZDF.SkinColor = ConfigSelection(default="default,#050a1232,#1502050e,#05192d7c", choices=ColorList)
 
