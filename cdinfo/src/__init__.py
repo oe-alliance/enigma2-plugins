263c9a56113bbb8b23af80459cd7a2dabeb53365
atvcaptain <openatv@gmail.com>
2026-04-04 14:40:59 +0200

    cdinfo update online server and test with mediaplayer

--- cdinfo/src/__init__.py
+++ cdinfo/src/__init__.py
@@ -1,2 +1,26 @@
+from __future__ import absolute_import
 
-__version__ = "1.0"
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import gettext
+
+
+PluginLanguageDomain = "CDInfo"
+PluginLanguagePath = "Extensions/CDInfo/locale"
+
+
+def localeInit():
+    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+
+def _(txt):
+    translated = gettext.dgettext(PluginLanguageDomain, txt)
+    if translated == txt:
+        translated = gettext.gettext(txt)
+    return translated
+
+
+localeInit()
+language.addCallback(localeInit)
+
+__version__ = "1.1"
